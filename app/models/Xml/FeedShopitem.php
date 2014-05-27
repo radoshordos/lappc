460b9f7e7bef19c3c77fff3c8f3593bd7448d533
radoshordos <radoshordos@gmail.com>
2014-05-27 11:47:31 +0200

    PPC XML FEED reader

---
+++ app/models/Xml/FeedShopitem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Authority\Xml;
+
+class FeedShopitem {
+
+    private $product;
+    private $url;
+    private $imgurl;
+    private $pricevat;
+
+    public function __construct() {
+
+    }s
+
+
+
+}
