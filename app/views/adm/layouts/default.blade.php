ab657ed67c2b6a8fd22c219ce22cbf5d09d9d05c
radoshordos <radoshordos@gmail.com>
2014-06-02 16:20:14 +0200

    Small fix rules

--- app/views/adm/layouts/default.blade.php
+++ app/views/adm/layouts/default.blade.php
@@ -52,7 +52,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">PPC <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="{{ URL::route('adm.ppc.rules.show')}}">PPC rules</a></li>
+                        <li><a href="{{ URL::route('adm.ppc.rules.index')}}">PPC rules</a></li>
                         <li><a href="{{ URL::to('adm/ppc/import') }}">Manual Import</a></li>
                         <li><a href="{{ URL::to('adm/ppc/config') }}">PPC Konfigurace</a></li>
                         <li><a href="{{ URL::to('adm/ppc/keywords') }}">PPC Keywords</a></li>
