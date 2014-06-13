66cc17f89fcd79512f8de554bcb8c558ce411d1d
radoshordos <radoshordos@gmail.com>
2014-06-13 10:08:18 +0200

    Add Triger

--- app/database/migrations/2014_06_12_100015_add_trigger_dev.php
+++ app/database/migrations/2014_06_12_100015_add_trigger_dev.php
@@ -7,9 +7,12 @@ class AddTriggerDev extends Migration
 
     public function up()
     {
+/*
         DB::unprepared('CREATE TRIGGER `dev_ai` AFTER INSERT ON `dev` FOR EACH ROW
                         BEGIN
 
+                        
+  WHERE dev_id = NEW.id;
                         UPDATE dev_group
                         SET dev_group.desc = (
                                                 SELECT GROUP_CONCAT(" ",dev.name)
@@ -21,25 +24,37 @@ class AddTriggerDev extends Migration
 
                         END;
         ');
-
+*/
         DB::unprepared('CREATE TRIGGER `dev_au` AFTER UPDATE ON `dev` FOR EACH ROW
                         BEGIN
 
-                        UPDATE dev_group
-                        SET dev_group.desc = (
-                                                SELECT GROUP_CONCAT(" ",dev.name)
-                                                FROM dev
-                                                INNER JOIN dev_m2n_group ON dev_m2n_group.dev_id = dev.id
-                                                WHERE dev_m2n_group.group_id IN (SELECT group_id FROM dev_m2n_group WHERE dev_id = NEW.id)
-                                                ORDER BY dev.id
-                                             );
-                        WHERE (SELECT group_id FROM dev_m2n_group WHERE dev_id = NEW.id)
+                        DECLARE count INT;
+                        DECLARE grupa INT;
+                        DECLARE my_cursor CURSOR FOR SELECT group_id FROM dev_m2n_group;
 
+                        SELECT group_id INTO count FROM dev_m2n_group;
 
+                        OPEN my_cursor;
 
+                        WHILE count > 0 DO
+                            FETCH my_cursor INTO group;
+
+                            UPDATE dev_group
+                            SET dev_group.desc = (
+                                                    SELECT GROUP_CONCAT(" ",dev.name)
+                                                    FROM dev
+                                                    INNER JOIN dev_m2n_group ON dev_m2n_group.dev_id = dev.id
+                                                    WHERE dev_m2n_group.group_id = grupa
+                                                    ORDER BY dev.id
+                                                 );
+                            WHERE dev_group.id = grupa;
+
+                            SET count = count - 1;
+                        END WHILE;
+                        CLOSE my_cursor;
                         END;
         ');
-
+/*
         DB::unprepared('CREATE TRIGGER `dev_ad` AFTER DELETE ON `dev` FOR EACH ROW
                         BEGIN
 
@@ -56,6 +71,7 @@ class AddTriggerDev extends Migration
 
                         END;;
         ');
+*/
     }
 
     public function down()
--- app/database/migrations/Trigges/dev.sql
+++ app/database/migrations/Trigges/dev.sql
@@ -39,3 +39,84 @@ SET dev_group.desc = dev_desc
 WHERE dev_group.id=NEW.id;
 
 END;
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddTriggerDev extends Migration
+{
+
+    public function up()
+    {
+/*
+        DB::unprepared('CREATE TRIGGER `dev_ai` AFTER INSERT ON `dev` FOR EACH ROW
+                        BEGIN
+
+
+  WHERE dev_id = NEW.id;
+                        UPDATE dev_group
+                        SET dev_group.desc = (
+                                                SELECT GROUP_CONCAT(" ",dev.name)
+                                                FROM dev
+                                                INNER JOIN dev_m2n_group ON dev_m2n_group.dev_id = dev.id
+                                                WHERE dev_m2n_group.group_id IN (SELECT group_id FROM dev_m2n_group WHERE dev_id = NEW.id)
+                                                ORDER BY dev.id
+                                             );
+
+                        END;
+        ');
+*/
+        DB::unprepared('CREATE TRIGGER `dev_au` AFTER UPDATE ON `dev` FOR EACH ROW
+                        BEGIN
+
+                        DECLARE count INT;
+                        DECLARE grupa INT;
+                        DECLARE my_cursor CURSOR FOR SELECT group_id FROM dev_m2n_group;
+
+                        SELECT group_id INTO count FROM dev_m2n_group;
+
+                        OPEN my_cursor;
+
+                        WHILE count > 0 DO
+                            FETCH my_cursor INTO group;
+
+                            UPDATE dev_group
+                            SET dev_group.desc = (
+                                                    SELECT GROUP_CONCAT(" ",dev.name)
+                                                    FROM dev
+                                                    INNER JOIN dev_m2n_group ON dev_m2n_group.dev_id = dev.id
+                                                    WHERE dev_m2n_group.group_id = grupa
+                                                    ORDER BY dev.id
+                                                 );
+                            WHERE dev_group.id = grupa;
+
+                            SET count = count - 1;
+                        END WHILE;
+                        CLOSE my_cursor;
+                        END;
+        ');
+/*
+        DB::unprepared('CREATE TRIGGER `dev_ad` AFTER DELETE ON `dev` FOR EACH ROW
+                        BEGIN
+
+                        DECLARE dtext VARCHAR(256);
+
+                        SELECT GROUP_CONCAT(" ",dev.name)
+                        INTO dtext
+                        FROM dev
+                        INNER JOIN dev_m2n_group ON dev_m2n_group.dev_id = dev.id
+                        WHERE dev_m2n_group.group_id = OLD.id
+                        ORDER BY dev.id;
+
+                        UPDATE dev_group SET dev_group.desc = dtext WHERE dev_group.id=OLD.id;
+
+                        END;;
+        ');
+*/
+    }
+
+    public function down()
+    {
+    }
+}
