f2a0d6159bc9a38c7b43ed2d31f7ef741c295e5e
radoshordos <radoshordos@gmail.com>
2014-06-24 10:00:35 +0200

    view prod

---
+++ app/database/migrations/2014_06_24_074005_view_prod.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ViewProd extends Migration {
+
+
+	public function up()
+	{
+
+        DB::unprepared('DROP TABLE IF EXISTS view_prod');
+
+        DB::unprepared('
+            CREATE OR REPLACE VIEW
+            view_prod AS
+            SELECT  prod.id AS prod_id,
+                    prod.name AS prod_name,
+                    prod.desc AS prod_desc,
+                    prod.price AS prod_price,
+                    tree.id AS tree_id,
+                    dev.id AS dev_id,
+                    dev.name AS dev_name
+            FROM    prod
+            INNER JOIN dev ON prod.dev_id = dev.id
+            INNER JOIN tree ON prod.tree_id = tree.id
+        ');
+	}
+
+	public function down()
+	{
+        DB::unprepared('DROP TABLE IF EXISTS view_prod');
+	}
+
+}
