85e80a711fc6d6f628b609b3d272b17b2d556fa4
Radek Horáček <radoshordos@gmail.com>
2014-07-05 20:27:52 +0200

    runner tree deep recalculate

--- app/database/migrations/2014_06_09_110000_dph.php
+++ app/database/migrations/2014_06_09_110000_dph.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ProdWarranty extends Migration {
+class Dph extends Migration {
 
     public function up()
     {
