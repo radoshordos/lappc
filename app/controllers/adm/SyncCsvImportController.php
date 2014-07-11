bd54c87366bb91ac4f19f2f3dfc543913ca9dbeb
Radek Horáček <radoshordos@gmail.com>
2014-07-11 23:49:39 +0200

    sync import

--- app/controllers/adm/SyncCsvImportController.php
+++ app/controllers/adm/SyncCsvImportController.php
@@ -4,11 +4,20 @@ use Authority\Tools\SB;
 
 class SyncCsvImportController extends \BaseController
 {
-
     public function create()
     {
         return View::make('adm.sync.csvimport.create',array(
-            'sync_template' => [''] + SB::option("SELECT * FROM sync_csv_template", ['id' => '->id'])
+            'sync_template' => [''] + SB::option('SELECT  sync_csv_template.id,
+                                                          sync_csv_template.purpose,
+                                                          mixture_dev.name,
+                                                          (SELECT GROUP_CONCAT("<",sync_csv_column.element,">")
+                                                            FROM sync_template_m2n_colmun
+                                                            INNER JOIN sync_csv_column ON sync_csv_column.id = sync_template_m2n_colmun.column_id
+                                                            WHERE sync_template_m2n_colmun.template_id = sync_csv_template.id
+                                                          ) AS list
+                                    FROM sync_csv_template
+                                    INNER JOIN mixture_dev ON mixture_dev.id = sync_csv_template.mixture_dev_id
+                                    ORDER BY mixture_dev_id,purpose', ['id' => ' [->name] [->purpose] ->list'])
         ));
     }
 
--- app/views/adm/sync/csvimport/create.blade.php
+++ app/views/adm/sync/csvimport/create.blade.php
@@ -6,18 +6,32 @@
 Import .csv dat
 @stop
 
+{{-- JavaScript on page --}}
+@section ('script')
+<script>
+    $(document).ready(function () {
+        $("#template_id").select2({});
+    });
+</script>
+@stop
+
 {{-- Content --}}
 @section('content')
 <div class="form-group">
-    {{ Form::label('template_id','Sablona',array('class'=> 'col-sm-2 control-label')) }}
-    <div class="col-sm-10">
+    {{ Form::label('template_id','Šablona',array('class'=> 'col-sm-2 control-label')) }}
+    <p class="col-sm-10">
         {{ Form::select('template_id', $sync_template, NULL, array('required' => 'required', 'class'=> 'form-control'))
         }}
-    </div>
+    </p>
 </div>
 <div class="form-group">
-    <div class="col-sm-12">
+    <p class="col-sm-12">
         {{ Form::textarea('notes', null, ['size' => '80x20', 'class' => 'form-control']) }}
-    </div>
+    </p>
 </div>
+
+<p class="text-center">
+    {{ link_to_route('adm.pattern.prod.index','Zkontolovat',NULL, array('class'=>'btn btn-primary','role'=> 'button')) }}
+
+</p>
 @stop
