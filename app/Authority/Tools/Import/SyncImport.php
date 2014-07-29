26eb1c55f97dd9519a9b68e93960f15fe2c0042f
radoshordos <radoshordos@gmail.com>
2014-07-29 16:58:36 +0200

    sync Import

--- app/Authority/Tools/Import/SyncImport.php
+++ app/Authority/Tools/Import/SyncImport.php
@@ -1,10 +1,26 @@
 <?php namespace Authority\Tools\Import;
 
-class SyncImport {
+use Authority\Eloquent\SyncCsvTemplate;
 
-    public function __construct($templateId, $separator , $data) {
+class SyncImport
+{
+    private $columns;
+    private $data;
+    private $separator;
 
-        echo 'ahoj';
+    public function __construct($templateId, $separator, $data)
+    {
+        $this->data = $data;
+        $this->separator = $separator;
+        $this->columns = $this->getColumnsName($templateId);
+    }
 
+    public function getColumnsName($templateId)
+    {
+        return \DB::table('sync_template_m2n_colmun')
+            ->leftJoin('sync_csv_column', 'sync_template_m2n_colmun.column_id', '=', 'sync_csv_column.id')
+            ->where('sync_template_m2n_colmun.template_id', '=', $templateId)
+            ->orderBy('sync_template_m2n_colmun.id')
+            ->get(array('sync_csv_column.element'));
     }
 }
