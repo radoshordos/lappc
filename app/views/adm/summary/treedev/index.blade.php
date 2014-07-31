731c40f4465daaadbe448f478fa3f2f04ea65ba6
radoshordos <radoshordos@gmail.com>
2014-07-31 13:45:15 +0200

    Výrobci ve skupinách

--- app/views/adm/summary/treedev/index.blade.php
+++ app/views/adm/summary/treedev/index.blade.php
@@ -11,12 +11,37 @@ Výrobci ve skupinách
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
         <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>Skupina</th>
+                <th><span title="Zanoření" class="glyphicon glyphicon-road"></span></th>
+                <th>DEV</th>
+                <th>
+                    <span title="Aktuální složka a podsložky, všechny produkty" class="glyphicon glyphicon-asterisk"></span>
+                </th>
+                <th>
+                    <span title="Aktuální složka a podsložky, jen viditelné produkty" class="glyphicon glyphicon-eye-open"></span>
+                </th>
+                <th>
+                    <span title="Aktuální složka, všechny produkty" class="glyphicon glyphicon-asterisk"></span>
+                </th>
+                <th>
+                    <span title="Aktuální složka, jen viditelné produkty" class="glyphicon glyphicon-eye-open"></span>
+                </th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <td colspan="7" class="active text-right">Počet záznamů: <b>{{ count($treedev) }}</b></td>
+            </tr>
+            </tfoot>
+            <tbody>
             @foreach ($treedev as $row)
-                @if ($row->dev_id == 1)
-                <tr class='success'>
-                    @else
-                <tr class='info'>
-                    @endif
+            @if ($row->dev_id == 1)
+            <tr class='success'>
+                @else
+            <tr class='info'>
+                @endif
                 <td>@if ($row->dev_id == 1) {{ $row->tree->name }} @endif</td>
                 <td>@if ($row->dev_id == 1) {{ $row->tree->deep }} @endif</td>
                 <td>{{ $row->dev->name }}</td>
@@ -26,6 +51,7 @@ Výrobci ve skupinách
                 <td>{{ $row->dir_visible }}</td>
             </tr>
             @endforeach
+            <tbody>
         </table>
     </div>
 </div>
