8752e69d817be8bb73ca648c74a94420e6d3c101
Radek Horáček <radoshordos@gmail.com>
2014-08-14 20:06:40 +0200

    Foudation ZURB

--- app/views/web/home.blade.php
+++ app/views/web/home.blade.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
-<html lang="cs">
+<!--[if IE 9]><html class="lt-ie10" lang="en"> <![endif]-->
+<html class="no-js" lang="en">
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
-    <link href="/web/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation 5</title>
+    <link rel="stylesheet" href="/components/foundation/css/normalize.css">
+    <link rel="stylesheet" href="/components/foundation/css/foundation.css">
+    <link rel="stylesheet" href="/components/foundation/css/app.css">
+    <script src="/components/modernizr/modernizr.js"></script>
 </head>
 <body>
-<h1>Hello, world!</h1>
+    <h1>Hello, world!</h1>
 
-<nav>
-    <ul>
-        <li><a href="">Akumulátorové nářadí</a></li>
-        <li><a href="">Elektrické nářadí</a></li>
-        <li><a href="">Vzduchové nářadí</a></li>
-        <li><a href="">Motorové nářadí</a></li>
-        <li><a href="">Ruční nářadí</a></li>
-        <li><a href="">Zahradní technika</a></li>
-        <li><a href="">Příslušenství</a></li>
-    </ul>
-</nav>
+    <nav>
+        <ul>
+            <li><a href="">Akumulátorové nářadí</a></li>
+            <li><a href="">Elektrické nářadí</a></li>
+            <li><a href="">Vzduchové nářadí</a></li>
+            <li><a href="">Motorové nářadí</a></li>
+            <li><a href="">Ruční nářadí</a></li>
+            <li><a href="">Zahradní technika</a></li>
+            <li><a href="">Příslušenství</a></li>
+        </ul>
+    </nav>
 
-<p><a href="http://localhost:8000/adm/">ADM</a></p>
-<p><a href="http://localhost:8000/adminer/">ADMINER</a></p>
+    <p><a href="http://localhost:8000/adm/">ADM</a></p>
+    <p><a href="http://localhost:8000/adminer/">ADMINER</a></p>
 
+    <!-- body content here -->
 
-
-<script src="/web/js/jquery-1.11.1.min.js"></script>
-<script src="/web/js/bootstrap.min.js"></script>
+    <script src="components/jquery/jquery.min.js"></script>
+    <script src="components/foundation/js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
 </body>
 </html>
