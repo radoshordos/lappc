b3b9f56a073b8f019bf4901f654ef8e80d980a8a
radoshordos <radoshordos@gmail.com>
2014-08-18 16:38:14 +0200

    components/font-awesome

--- app/views/web/home.blade.php
+++ app/views/web/home.blade.php
@@ -14,13 +14,50 @@
 <body>
     <h1>Hello, world!</h1>
 
-        <div id="container">
+    <div id="container">
+            <div class="row">
+                <div class="small-12 columns">
+                    <nav class="top-bar" data-topbar>
+                        <ul class="title-area">
+                            <li class="name">
+                                <h1><a href="#"></a></h1>
+                            </li>
+                        </ul>
+                        <section class="top-bar-section">
+                            <ul class="right">
+                                <li class="has-dropdown">
+                                    <a href="#">Right Button Dropdown</a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">First link in dropdown</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <!-- Left Nav Section -->
+                            <ul class="left">
+                                <li class="has-form">
+                                    <div class="row collapse">
+                                        <div class="large-8 small-9 columns">
+                                            <input type="search" size="40" placeholder="Nalést nářadí a příslušenství">
+                                        </div>
+                                        <div class="large-4 small-3 columns">
+                                            <a href="#" class="alert button expand">Hledat</a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </section>
+                    </nav>
+                    <ul class="breadcrumbs">
+                        <li><a href="#">Úvod</a></li>
+                        <li><a href="#">Akumulátorové nářadí</a></li>
+                        <li><a href="#">Aku kladiva</a></li>
+                    </ul>
+                </div>
+            </div>
 
-            <ul class="breadcrumbs">
-                <li><a href="#">Úvod</a></li>
-                <li><a href="#">Akumulátorové nářadí</a></li>
-                <li><a href="#">Aku kladiva</a></li>
-            </ul>
+
+
+
 
             <div class="row">
                 <div class="small-3 columns">
