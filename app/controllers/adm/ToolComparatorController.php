a06493671a66fb212ac561b8c717185d9a5037d1
radoshordos <radoshordos@gmail.com>
2014-09-04 12:10:06 +0200

    comparator fix

--- app/controllers/adm/ToolComparatorController.php
+++ app/controllers/adm/ToolComparatorController.php
@@ -4,25 +4,28 @@ class ToolComparatorController extends \BaseController
 {
     public function index()
     {
-
         $one = $this->toArr(Input::get('one'));
         $two = $this->toArr(Input::get('two'));
+        $diff = array_filter(array_diff($one, $two));
+        $intersect = array_filter(array_intersect($one, $two));
 
         return View::make('adm.tools.comparator.index', array(
             'one' => Input::get('one'),
             'two' => Input::get('two'),
-            'diff' => $this->toStr(array_diff($one,$two)),
-            'intersect' => $this->toStr(array_intersect($one,$two)),
-            'diff_count' => count(array_diff($one,$two)),
-            'intersect_count' => count(array_intersect($one,$two))
+            'diff' => $this->toStr($diff),
+            'intersect' => $this->toStr($intersect),
+            'diff_count' => count($diff),
+            'intersect_count' => count($intersect)
         ));
     }
 
-    private function toArr($str) {
-        return explode("\r\n",$str);
+    private function toArr($str)
+    {
+        return explode("\r\n", $str);
     }
 
-    private function toStr(array $arr) {
-        return implode("\r\n",$arr);
+    private function toStr(array $arr)
+    {
+        return trim(implode("\r\n", $arr));
     }
 }
