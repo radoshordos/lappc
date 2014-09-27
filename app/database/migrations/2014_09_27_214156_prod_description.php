7635236f134d7cbbf0cd2d36b496798812f28ee7
Radek Horáček <radoshordos@gmail.com>
2014-09-27 23:59:12 +0200

    prod description

---
+++ app/database/migrations/2014_09_27_214156_prod_description.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProdDescription extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('prod_description', function (Blueprint $table) {
+
+            $table->increments('id')->unsigned();
+            $table->integer('prod_id')->unsigned();
+            $table->smallInteger('variations_id')->unsigned();
+            $table->text('data');
+
+            $table->engine = 'InnoDB';
+            $table->foreign('prod_id')->references('id')->on('prod')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('prod_description');
+    }
+
+}
---
+++ app/database/migrations/2014_09_27_214647_media_variations.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MediaVariations extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('media_variations', function (Blueprint $table) {
+
+            $table->smallInteger('id')->unsigned()->primary();
+            $table->tinyInteger('type_id')->unsigned();
+            $table->boolean('visible_rule')->default(1);
+            $table->boolean('visible_group')->default(0);
+            $table->boolean('visible_media')->default(0);
+            $table->boolean('visible_prod')->default(0);
+            $table->string('tag', 24)->nullable();
+            $table->string('class', 16)->nullable();
+            $table->string('icon', 32)->nullable();
+            $table->string('name', 48);
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('media_variations');
+    }
+
+}
