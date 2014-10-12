be56ea5fde51255565be7fada04775b04e848eb5
Radek Horáček <radoshordos@gmail.com>
2014-10-12 23:41:46 +0200

    Simulace filtru

--- app/controllers/adm/GrabSimulatorController.php
+++ app/controllers/adm/GrabSimulatorController.php
@@ -1,11 +1,18 @@
 <?php
 
+use Authority\Eloquent\GrabProfile;
+use Authority\Tools\SB;
+
 class GrabSimulatorController extends \BaseController
 {
 
     public function index()
     {
-        return View::make('adm.tools.grabsimulator.index', []);
+        return View::make('adm.tools.grabsimulator.index', [
+            'choise_profile_id' => intval(Input::get('profile_id')),
+            'profile_url' => Input::get('profile_url'),
+            'profile_id' => [''] + SB::option("SELECT * FROM grab_profile WHERE active = 1 ORDER BY name", ['id' => '->name']),
+        ]);
     }
 
 }
--- app/views/adm/tools/grabsimulator/index.blade.php
+++ app/views/adm/tools/grabsimulator/index.blade.php
@@ -3,9 +3,46 @@
 {{-- Web site Title --}}
 @section('title')
 @parent
-GRAB Simulator
+Simulace filtru
 @stop
 
 {{-- Content --}}
 @section('content')
+{{ Form::open(['route' => ['adm.tools.grabsimulator.index'],'method' => 'GET','class' => 'form-horizontal', 'role' => 'form']) }}
+<div class="panel panel-default">
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="input-group">
+                    <span class="input-group-addon">URL</span>
+                    {{ Form::url('profile_url', $profile_url, ["maxlength" => "255",'class'=> 'form-control']) }}
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="input-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">Profil</span>
+                        {{ Form::select('profile_id', $profile_id, $choise_profile_id, ['id' => 'profile_id', 'class'=> 'form-control']) }}
+                        <span class="input-group-btn">
+                            {{ Form::submit('Go!',['class' => 'btn btn-success'])  }}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{ Form::close() }}
+
+@if ($profile_id > 0)
+<table>
+    <tbody>
+        <tr>
+            <td>{{ Form::textarea('reading', NULL, ['size' => '80x35','class'=> 'form-control']); }}</td>
+            <td>{{ Form::textarea('results', NULL, ['size' => '80x35','class'=> 'form-control']); }}</td>
+        </tr>
+    </tbody>
+</table>
+@endif
+
 @stop
