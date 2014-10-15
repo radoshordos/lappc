be626358a60555c716989f08528fdc7be7af918e
radoshordos <radoshordos@gmail.com>
2014-10-15 17:00:47 +0200

    Seed Prod Difference fix

--- app/database/seeds/ProdDifferenceN2mSetSeeder.php
+++ app/database/seeds/ProdDifferenceN2mSetSeeder.php
@@ -5,24 +5,12 @@ class ProdDifferenceN2mSetSeeder extends Seeder
 
     public function run()
     {
-        /*
         $prod2difference2in2set = [
-            ['pdis_id' => '1', 'pdis_id_difference' => '10', 'pdis_id_set' => '11'],
-            ['pdis_id' => '2', 'pdis_id_difference' => '103', 'pdis_id_set' => '11'],
-            ['pdis_id' => '3', 'pdis_id_difference' => '103', 'pdis_id_set' => '50'],
-            ['pdis_id' => '4', 'pdis_id_difference' => '105', 'pdis_id_set' => '40'],
-            ['pdis_id' => '5', 'pdis_id_difference' => '105', 'pdis_id_set' => '50']
-            ['pdis_id' => '6', 'pdis_id_difference' => '20', 'pdis_id_set' => '1'],
+            ['pdis_id' => '1', 'pdis_id_difference' => '11', 'pdis_id_set' => '11'],
+            ['pdis_id' => '2', 'pdis_id_difference' => '13', 'pdis_id_set' => '13'],
+            ['pdis_id' => '3', 'pdis_id_difference' => '143', 'pdis_id_set' => '11'],
+            ['pdis_id' => '4', 'pdis_id_difference' => '143', 'pdis_id_set' => '13'],
         ];
-
-            ['pds_id' => '1', 'pds_visible' => '1', 'pds_name' => '[A/N]', 'pds_sortby' => 'id'],
-            ['pds_id' => '5', 'pds_visible' => '1', 'pds_name' => 'Velikost', 'pds_sortby' => 'id'],
-            ['pds_id' => '7', 'pds_visible' => '1', 'pds_name' => 'Barva', 'pds_sortby' => 'name'],
-            ['pds_id' => '11', 'pds_visible' => '1', 'pds_name' => 'Rozměr', 'pds_sortby' => 'id'],
-            ['pds_id' => '13', 'pds_visible' => '1', 'pds_name' => 'Délka', 'pds_sortby' => 'id'],
-
-
-
 
         DB::table('prod_difference_n2m_set')->delete();
 
@@ -33,8 +21,6 @@ class ProdDifferenceN2mSetSeeder extends Seeder
                 'difference_id' => $row['pdis_id_difference'],
                 'set_id'        => $row['pdis_id_set']
             ]);
-        }*/
+        }
     }
-
-
 }
