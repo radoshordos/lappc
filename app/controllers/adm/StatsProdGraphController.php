3a364eb1934711338b02abf9b62096b9f3aafe76
radoshordos <radoshordos@gmail.com>
2014-10-17 10:58:49 +0200

    Graph

---
+++ app/controllers/adm/StatsProdGraphController.php
@@ -0,0 +1,11 @@
+<?php
+
+class StatsProdGraphController extends \BaseController
+{
+    public function index()
+    {
+        return View::make('adm.stats.prodgraph.index', [
+
+        ]);
+    }
+}
