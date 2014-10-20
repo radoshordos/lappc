06fe25cacc009e8c60f56f9a8d233d0ef498397f
Radek Horáček <radoshordos@gmail.com>
2014-10-20 23:48:14 +0200

    Hromadné změny prouktů data

--- app/controllers/adm/ProdMultipleChangesController.php
+++ app/controllers/adm/ProdMultipleChangesController.php
@@ -7,6 +7,19 @@ class ProdMultipleChangesController extends \BaseController
 
     public function index()
     {
+        if (Input::has('multi-change') && Input::get('select_dev') > 1) {
+            $db = \DB::table('items');
+            $db->join('prod', 'prod.id', '=', 'items.prod_id');
+            $db->select('items.id');
+            $db->where('prod_id > 0');
+            $db->where('prod.dev_id', '=', Input::get('select_dev'));
+            (Input::get('select_tree') > 0) ? $db->where('prod.id_tree', '=', Input::get('select_tree')) : NULL;
+            (Input::get('old_select_sale') > 0) ? $db->where('items.sale_id', '=', Input::get('old_select_sale')) : NULL;
+            (Input::get('old_select_availability') > 0) ? $db->where('items.availability_id', '=', Input::get('old_select_availability')) : NULL;
+            $row = $db->get('items.id');
+            var_dump($row);
+        }
+
         return View::make('adm.pattern.multiplechanges.index', [
             'select_dev'                     => SB::option('SELECT * FROM dev WHERE active = 1 ORDER BY id', ['id' => '[->id] - ->name']),
             'select_tree'                    => SB::option('SELECT * FROM view_tree WHERE tree_dir_all > 0', ['tree_id' => '[->tree_id] - [->tree_absolute] - ->tree_name'], TRUE),
@@ -41,7 +54,6 @@ class Zbozi_Multiple2changesController extends Zend_Controller_Action {
                 ->where(((intval($request->getParam("prod_id_tree")) > 0) ? $db->quoteInto("prod_id_tree = ?", intval($request->getParam("prod_id_tree"))) : "1=1"))
                 ->where(((strlen($request->getParam("old_id_sale")) > 0) ? $db->quoteInto("items_id_sale = ?", intval($request->getParam("old_id_sale"))) : "1=1"))
                 ->where(((strlen($request->getParam("old_id_avib")) > 0) ? $db->quoteInto("items_id_availibility = ?", intval($request->getParam("old_id_avib"))) : "1=1"))
-                ->where(((strlen($request->getParam("old_online")) > 0) ? $db->quoteInto("prod_is_online = ?", intval($request->getParam("old_online"))) : "1=1"))
                 ->where("prod_id_dev = ?", intval($request->getParam("prod_id_dev")));
 
             $list_items = $db->query($select)->fetchAll();
@@ -54,9 +66,7 @@ class Zbozi_Multiple2changesController extends Zend_Controller_Action {
                 $md->multipleAvability($list_items);
             }
 
-            if (strlen($request->getParam("old_online")) > 0 && strlen($request->getParam("new_online")) > 0) {
-                $md->multipleOnlineSelling($list_items);
-            }
+
         }
     }
 
