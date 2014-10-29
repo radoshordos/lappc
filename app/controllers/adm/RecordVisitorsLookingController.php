63f598142a1898a0de11a407d07aca5acef92518
Radek Horáček <radoshordos@gmail.com>
2014-10-29 22:17:06 +0100

    triple tabs visitors

--- app/controllers/adm/RecordVisitorsLookingController.php
+++ app/controllers/adm/RecordVisitorsLookingController.php
@@ -10,4 +10,44 @@ class RecordVisitorsLookingController extends \BaseController
             'recordvisitors' => RecordVisitorsLooking::orderBy('id','DESC')->get()
         ]);
     }
-} 
+}
+
+/*
+ *
+ if (intval($_GET['domain']) > 0) {
+    $select->where("lpf_id_domain = ?", intval($_GET['domain']));
+}
+if (!empty($_GET['lpf_filter_find'])) {
+    $select->where("lpf_filter_find LIKE ?", "%" . trim(strip_tags($_GET['lpf_filter_find'])) . "%");
+}
+
+switch (intval($_GET["type"])) {
+    case 2:
+        $res = $db->fetchAll($select
+                        ->from("log2prod2find", array("lpf_filter_find", "lpf_result_count_dev", "lpf_result_count_prod", "pocet" => "COUNT(log2prod2find.lpf_id)"))
+                        ->joinLeft("domain", Model_Zendb::ZEND_LOG2PROD2FIND_2_DOMAIN, array("domain_name"))
+                        ->group("lpf_filter_find")
+                        ->limit(((intval($_GET["limit"]) > 0)) ? intval($_GET["limit"]) : 40)
+                        ->order(array("pocet DESC", "lpf_id DESC"))
+        );
+        break;
+    case 3:
+        $res = $db->fetchAll($select
+                        ->from("log2prod2find", array("lpf_filter_find", "lpf_result_count_dev", "lpf_result_count_prod", "pocet" => "COUNT(lpf_id)"))
+                        ->joinLeft("domain", Model_Zendb::ZEND_LOG2PROD2FIND_2_DOMAIN, array("domain_name"))
+                        ->group("lpf_filter_find")
+                        ->where("lpf_result_count_prod = 0")
+                        ->order(array("pocet DESC", "lpf_id DESC"))
+                        ->limit(((intval($_GET["limit"]) > 0)) ? intval($_GET["limit"]) : 40)
+        );
+        break;
+    default:
+        $res = $db->fetchAll($select
+                        ->from("log2prod2find", array("lpf_filter_find", "lpf_ti_create", "lpf_result_count_dev", "lpf_result_count_prod"))
+                        ->joinLeft("domain", Model_Zendb::ZEND_LOG2PROD2FIND_2_DOMAIN, array("domain_name"))
+                        ->order("lpf_id DESC")
+                        ->limit(((intval($_GET["limit"]) > 0)) ? intval($_GET["limit"]) : 40)
+        );
+        break;
+}
+ */
--- app/views/adm/stats/recordvisitors/index.blade.php
+++ app/views/adm/stats/recordvisitors/index.blade.php
@@ -8,31 +8,96 @@ Uživatelé hledají
 
 {{-- Content --}}
 @section('content')
-<table class="table table-bordered table-striped table-condensed table-hover">
-    <thead>
-        <tr>
-            <th class="text-center">#ID</th>
-            <th>Datum a čas</th>
-            <th>Hledaný výraz</th>
-            <th>Počet produktů</th>
-            <th>Počet výrobců</th>
-        </tr>
-    </thead>
-    <tfoot>
-        <tr>
-             <td colspan="5" class="text-right">Zobrazeno : <strong><?= count($recordvisitors) ?></strong> výsledků</td>
-        </tr>
-    </tfoot>
-    <tbody>
-        @foreach ($recordvisitors as $row)
-        <tr>
-            <td>{{ $row->id; }}</td>
-            <td>{{ $row->find_at; }}</td>
-            <td>{{ $row->filter_find; }}</td>
-            <td>{{ $row->count_prod; }}</td>
-            <td>{{ $row->count_dev; }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+
+<ul class="nav nav-tabs" role="tablist">
+  <li class="active"><a href="#last" role="tab" data-toggle="tab">Poslední hledané výrazy</a></li>
+  <li><a href="#favorite" role="tab" data-toggle="tab">Nejčasněji hledané výrazy</a></li>
+  <li><a href="#zero" role="tab" data-toggle="tab">Nejčasněji nenalezené výrazy</a></li>
+</ul>
+
+<div class="tab-content">
+    <div class="tab-pane fade in active" id="last" style="padding-top: 2em">
+        <table class="table table-bordered table-striped table-condensed table-hover">
+            <thead>
+                <tr>
+                    <th class="text-center">#ID</th>
+                    <th>Datum a čas</th>
+                    <th>Hledaný výraz</th>
+                    <th>Počet produktů</th>
+                    <th>Počet výrobců</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                     <td colspan="5" class="text-right">Zobrazeno : <strong>{{ count($recordvisitors) }}</strong> výsledků</td>
+                </tr>
+            </tfoot>
+            <tbody>
+                @foreach ($recordvisitors as $row)
+                <tr>
+                    <td>{{ $row->id; }}</td>
+                    <td>{{ $row->find_at; }}</td>
+                    <td>{{ $row->filter_find; }}</td>
+                    <td>{{ $row->count_prod; }}</td>
+                    <td>{{ $row->count_dev; }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="tab-pane fade in active" id="favorite" style="padding-top: 2em">
+    <table>
+        <thead>
+            <tr class="center">
+                <th>PoÄ?et</th>
+                <th>HledanĂ˝ vĂ˝raz</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <td colspan="3" class="right">Zobrazeno : <strong><?= count($res) ?></strong> Ĺ?ĂĄdkĹŻ</td>
+            </tr>
+        </tfoot>
+        <tbody>
+                @foreach ($recordvisitors as $row)
+                    <tr>
+                        <td class="center"><?= $row->pocet; ?></td>
+                        <td><?= $row->lpf_filter_find; ?></td>
+                    </tr>
+                @endforeach
+        </tbody>
+    </table>
+
+
+    </div>
+    <div class="tab-pane fade in active" id="zero" style="padding-top: 2em">
+
+
+        <table>
+            <thead>
+                <tr class="center">
+                    <th>PoÄ?et</th>
+                    <th>HledanĂ˝ vĂ˝raz</th>
+                    <th>SUMA</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <td colspan="4" class="right">Zobrazeno : <strong><?= count($res) ?></strong> Ĺ?ĂĄdkĹŻ</td>
+                </tr>
+            </tfoot>
+                @foreach ($recordvisitors as $row)
+                    <tbody>
+                        <tr>
+                            <td class="center"><?= $row->pocet; ?></td>
+                            <td><?= $row->lpf_filter_find ?></td>
+                            <td class="center"><?= $row->lpf_result_count_prod; ?></td>
+                        </tr>
+                    </tbody>
+                @endforeach
+        </table>
+
+        
+    </div>
+</div>
 @stop
