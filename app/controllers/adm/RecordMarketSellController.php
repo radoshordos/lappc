f4ef2b95294a45d750941bd074d4b5fd48f14ecb
radoshordos <radoshordos@gmail.com>
2014-10-31 16:07:14 +0100

    Prodeje v obchodu graph

--- app/controllers/adm/RecordMarketSellController.php
+++ app/controllers/adm/RecordMarketSellController.php
@@ -6,31 +6,36 @@ class RecordMarketSellController extends \BaseController
 {
     public function index()
     {
+        $row = [];
+        if (Input::has('submit_month_record')) {
+            $row = RecordMarketSell::select([
+                '*',
+                DB::raw('(SELECT price_all - price_transport) AS result_price_only'),
+                DB::raw('(SELECT price_all / count_buy_success) AS result_price_prumer')
+            ])
+                ->where('month', '>=', Input::get('month-start') . "-01")
+                ->where('month', '<=', Input::get('month-end') . "-01")
+                ->orderBy('month')
+                ->get();
+        }
+
         return View::make('adm.stats.marketsell.index', [
-            'choice-month-start' => Input::get('month-start'),
-            'choice-month-end'   => Input::get('month-end'),
+            'source' => $row,
+            'choice_month_start' => Input::get('month-start'),
+            'choice_month_end'   => Input::get('month-end'),
             'max_insert_month'   => substr(RecordMarketSell::max('month'), 0, 7),
             'min_insert_month'   => substr(RecordMarketSell::min('month'), 0, 7)
         ]);
     }
 }
 
+
+
 /*
-$inc = 0;
-$max_insert_month = substr($db->fetchOne($db->select()->from("log2market", array("MAX(lm_month)"))), 0, 7);
-$min_insert_month = substr($db->fetchOne($db->select()->from("log2market", array("MIN(lm_month)"))), 0, 7);
 
 if (!empty($_POST["submit-month-record"])) {
 
-    $row = $db->fetchAll($db->select()->from("log2market", array(
-            "*",
-            "result_price_only" => "(SELECT lm_price_all - lm_price_transport)",
-            "result_price_prumer" => "(SELECT lm_price_all / lm_count_buy_success)"
-        ))
-            ->where("lm_month >= ?", $_POST["month-start"] . "-01")
-            ->where("lm_month <= ?", $_POST["month-end"] . "-01")
-            ->order(array("lm_month"))
-    );
+
 
     $sum = $db->fetchRow($db->select()->from("log2market", array(
         "sum_count_row" => "(SELECT COUNT(lm_month))",
--- app/views/adm/stats/marketsell/index.blade.php
+++ app/views/adm/stats/marketsell/index.blade.php
@@ -6,49 +6,62 @@
 Statistiky produktů
 @stop
 
+{{-- JavaScript on page --}}
+@section ('script')
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['M\u011bsíc', 'Cena celkem s dopravou', 'Cena celkem','Doprava'],
+          @foreach($source as $val)
+          [{{substr($val->_month, 0, 7)}},  {{ $val->price_all }},  {{ $val->result_price_only }},  {{ $val->price_transport }}],
+          @endforeach
+        ]);
+        var options = {
+          title: 'Prodeje obchodu'
+        };
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+</script>
+@stop
+
 {{-- Content --}}
 @section('content')
-<form action="" method="post">
-<table>
+{{ Form::open(['route' => ['adm.stats.marketsell.index'], 'method' => 'GET', 'class' => 'form-horizontal', 'role' => 'form']) }}
+<div class="row">
+<div class="col-md-6 col-md-offset-3">
+<table class="table">
+    <thead>
+        <tr>
+            <th class="text-center">{{ Form::label('month-start','Počátek')  }}</th>
+            <th class="text-center">{{ Form::label('month-start','Konec')  }}</th>
+            <th></th>
+        </tr>
+    </thead>
     <tbody>
         <tr>
-            <td>{{ Form::selectMonth('month-start',['min'=> $min_insert_month,'max'=> $max_insert_month, 'value' => (!empty($_POST["month-start"]) ? $_POST["month-start"] : $min_insert_month);) }}</td>
-            <td>{{ Form::selectMonth('month-end',['min'=> $min_insert_month,'max'=> $max_insert_month, 'value' => $max_insert_month]) }}</td>
-            <td>{{ Form::submit("Provést",["name" => "submit-month-record"]) }}</td>
+            <td>{{ Form::input('month', 'month-start', (isset($choice_month_start) ? $choice_month_start : $min_insert_month), ['min'=> $min_insert_month,'max'=> $max_insert_month,'class'=> 'form-control']); }}</td>
+            <td>{{ Form::input('month', 'month-end', (isset($choice_month_end) ? $choice_month_end : $max_insert_month), ['min'=> $min_insert_month,'max'=> $max_insert_month,'class'=> 'form-control']); }}</td>
+            <td>{{ Form::submit('Provést',['name' => 'submit_month_record','class' => 'btn btn-primary']) }}</td>
         </tr>
     </tbody>
 </table>
-</form>
+</div>
+</div>
+{{ Form::close() }}
+<div id="chart_div" style="width: 1200px; height: 600px;"></div>
 @stop
 
 
-            <td><input type="month" name="month-start" min="<?= $min_insert_month ?>" max="<?= $max_insert_month ?>" value="<?= (!empty($_POST["month-start"]) ? $_POST["month-start"] : $min_insert_month); ?>" /></td>
-            <td><input type="month" name="month-end"  min="<?= $min_insert_month ?>"  max="<?= $max_insert_month ?>" value="<?= (!empty($_POST["month-end"]) ? $_POST["month-end"] : $max_insert_month); ?>" /></td>
-            <td><?= $this->formSubmit("submit-month-record", "Provést"); ?></td>
-
-
+<!-- /*
 
             <script type="text/javascript">
-                google.load("visualization", "1", {packages:["corechart"]});
-                google.setOnLoadCallback(drawChart);
-                function drawChart() {
-                    var data = new google.visualization.DataTable();
-                    data.addColumn('string', 'M\u011bsíc');
-                    data.addColumn('number', 'Cena celkem s dopravou');
-                    data.addColumn('number', 'Cena celkem');
-                    data.addColumn('number', 'Doprava');
-                    data.addRows(<?= count($row) ?>);
-<?php foreach ($row as $val) { ?>
-            data.setValue(<?= $inc ?>, 0, '<?= substr($val->lm_month, 0, 7) ?>');
-            data.setValue(<?= $inc ?>, 1, <?= $val->lm_price_all ?>);
-            data.setValue(<?= $inc ?>, 2, <?= $val->result_price_only ?>);
-            data.setValue(<?= $inc++ ?>, 3, <?= $val->lm_price_transport ?>);
-<?php } ?>
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, {width: 1024, height: 480, title: 'Prodeje obchodu'});
-    }
+
             </script>
-            <div id="chart_div"></div>
+
 
 
 
@@ -67,35 +80,37 @@ Statistiky produktů
                     <tfoot>
                         <tr class="right">
                             <th>SUMA</th>
-                            <td><?= $sum->sum_month_count_buy_all; ?></td>
-                            <td><?= $sum->sum_month_count_buy_success; ?></td>
+                            <td>< $sum->sum_month_count_buy_all; ?></td>
+                            <td>< $sum->sum_month_count_buy_success; ?></td>
                             <td>A/N</td>
-                            <td><?= $sum->sum_month_price_all; ?></td>
-                            <td><?= $sum->sum_month_price_only; ?></td>
-                            <td><?= $sum->sum_month_price_transport; ?></td>
+                            <td>< $sum->sum_month_price_all; ?></td>
+                            <td>< $sum->sum_month_price_only; ?></td>
+                            <td>< $sum->sum_month_price_transport; ?></td>
                         </tr>
                         <tr class="right">
                             <th>PRŮMER</th>
-                            <td><?= round(($sum->sum_month_count_buy_all / $sum->sum_count_row), 0); ?></td>
-                            <td><?= round(($sum->sum_month_count_buy_success / $sum->sum_count_row), 0); ?></td>
-                            <td><?= round(($sum->sum_month_price_all / $sum->sum_month_count_buy_success), -1); ?></td>
-                            <td><?= round(($sum->sum_month_price_all / $sum->sum_count_row), 0); ?></td>
-                            <td><?= round(($sum->sum_month_price_only / $sum->sum_count_row), 0); ?></td>
-                            <td><?= round(($sum->sum_month_price_transport / $sum->sum_count_row), 0); ?></td>
+                            <td>< round(($sum->sum_month_count_buy_all / $sum->sum_count_row), 0); ?></td>
+                            <td>< round(($sum->sum_month_count_buy_success / $sum->sum_count_row), 0); ?></td>
+                            <td>< round(($sum->sum_month_price_all / $sum->sum_month_count_buy_success), -1); ?></td>
+                            <td>< round(($sum->sum_month_price_all / $sum->sum_count_row), 0); ?></td>
+                            <td>< round(($sum->sum_month_price_only / $sum->sum_count_row), 0); ?></td>
+                            <td>< round(($sum->sum_month_price_transport / $sum->sum_count_row), 0); ?></td>
                         </tr>
                     </tfoot>
                     <tbody>
-                        <?php foreach ($row as $v) { ?>
+
                             <tr class="right">
-                                <td><?= substr($v->lm_month, 0, 7) ?></td>
-                                <td><?= $v->lm_count_buy_all ?></td>
-                                <td><?= $v->lm_count_buy_success ?></td>
-                                <td><?= round($v->result_price_prumer, -1) ?></td>
-                                <td><?= $v->lm_price_all ?></td>
-                                <td><?= $v->result_price_only ?></td>
-                                <td><?= $v->lm_price_transport ?></td>
+                                <td> substr($v->lm_month, 0, 7) </td>
+                                <td> $v->lm_count_buy_all </td>
+                                <td> $v->lm_count_buy_success </td>
+                                <td> round($v->result_price_prumer, -1) </td>
+                                <td> $v->lm_price_all </td>
+                                <td> $v->result_price_only </td>
+                                <td> $v->lm_price_transport </td>
                             </tr>
-                        <?php } ?>
                     </tbody>
                 </table>
 
+
+
+ */ -->
