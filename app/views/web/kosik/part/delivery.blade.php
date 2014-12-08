a0ed244623629a2d41b46a974578436c52e9e377
Radek Horáček <radoshordos@gmail.com>
2014-12-08 23:25:09 +0100

    fieldset

--- app/views/web/kosik/part/delivery.blade.php
+++ app/views/web/kosik/part/delivery.blade.php
@@ -1,28 +1,30 @@
-TRANSPORT
-<div class="row">
-    <div class="large-11 columns small-centered">
-        <input id="zasilkovna" type="radio" autocomplete="off" name="deliveryId" value="">
-        <label for="zasilkovna">Osobní odběr - Zásilkovna</label>
+<fieldset>
+    <legend>TRANSPORT</legend>
+    <div class="row">
+        <div class="large-11 columns small-centered">
+            <input id="zasilkovna" type="radio" autocomplete="off" name="deliveryId" value="">
+            <label for="zasilkovna">Osobní odběr - Zásilkovna</label>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="large-11 columns small-centered">
-        <input id="osobne" type="radio" autocomplete="off" name="deliveryId" value="">
-        <label for="osobne">Osobní odběr - V obchodě (Benešov)</label>
+    <div class="row">
+        <div class="large-11 columns small-centered">
+            <input id="osobne" type="radio" autocomplete="off" name="deliveryId" value="">
+            <label for="osobne">Osobní odběr - V obchodě (Benešov)</label>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="large-11 columns small-centered">
-        <input id="balik" type="radio" autocomplete="off" name="deliveryId" value="">
-        <label for="balik">Balík</label>
+    <div class="row">
+        <div class="large-11 columns small-centered">
+            <input id="balik" type="radio" autocomplete="off" name="deliveryId" value="">
+            <label for="balik">Balík</label>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="large-11 columns small-centered">
-        <input id="dobirka" type="radio" autocomplete="off" name="deliveryId" value="">
-        <label for="dobirka">Dobírka</label>
+    <div class="row">
+        <div class="large-11 columns small-centered">
+            <input id="dobirka" type="radio" autocomplete="off" name="deliveryId" value="">
+            <label for="dobirka">Dobírka</label>
+        </div>
     </div>
-</div>
+</fieldset>
