326bfe10ed40fdcf9a49fdbb3b32eff5915577b5
Radek Horáček <radoshordos@gmail.com>
2014-12-08 23:07:17 +0100

    TRANSPORT and PLATBA

---
+++ app/views/web/kosik/part/delivery.blade.php
@@ -0,0 +1,28 @@
+TRANSPORT
+<div class="row">
+    <div class="large-11 columns small-centered">
+        <input id="zasilkovna" type="radio" autocomplete="off" name="deliveryId" value="">
+        <label for="zasilkovna">Osobní odběr - Zásilkovna</label>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-11 columns small-centered">
+        <input id="osobne" type="radio" autocomplete="off" name="deliveryId" value="">
+        <label for="osobne">Osobní odběr - V obchodě (Benešov)</label>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-11 columns small-centered">
+        <input id="balik" type="radio" autocomplete="off" name="deliveryId" value="">
+        <label for="balik">Balík</label>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-11 columns small-centered">
+        <input id="dobirka" type="radio" autocomplete="off" name="deliveryId" value="">
+        <label for="dobirka">Dobírka</label>
+    </div>
+</div>
