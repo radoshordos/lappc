4c00e26186538645fabbff30fffe7f8c3f14d4cb
Radek Horáček <radoshordos@gmail.com>
2014-12-08 19:36:47 +0100

    import optimalization

--- app/Authority/Eloquent/Items.php
+++ app/Authority/Eloquent/Items.php
@@ -6,6 +6,12 @@ class Items extends \Eloquent
     protected $table = 'items';
     protected $guarded = [];
 
+    public static $rules = [
+        'id'        => 'required|integer',
+        'code_prod' => "unique:items,code_prod",
+        'code_ean'  => "unique:items,code_ean",
+    ];
+
     public function itemsAvailability()
     {
         return $this->hasOne('Authority\Eloquent\ItemsAvailability', 'id', 'availability_id');
