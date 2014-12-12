2b7ef3156bafcd2f2e4f320e6830e90611e0232a
Radek Horáček <radoshordos@gmail.com>
2014-12-12 09:42:12 +0100

    purpose = "manualsync" OR "autosync"

--- app/controllers/adm/SyncSummaryController.php
+++ app/controllers/adm/SyncSummaryController.php
@@ -9,8 +9,8 @@ class SyncSummaryController extends \BaseController
 		return View::make('adm.sync.summary.index', [
 			'summary' => SyncDb::selectRaw(
 					'dev.name as dev_name,
-					(SELECT COUNT(*) FROM sync_db WHERE sync_db.item_id IS NOT NULL AND sync_db.dev_id = dev.id) AS count_insert_prod,
-					(SELECT COUNT(*) FROM sync_db WHERE sync_db.dev_id = dev.id) AS count_items_dev'
+					(SELECT COUNT(*) FROM sync_db WHERE sync_db.item_id IS NOT NULL AND sync_db.dev_id = dev.id AND sync_db.purpose = "manualsync" OR sync_db.purpose = "autosync") AS count_insert_prod,
+					(SELECT COUNT(*) FROM sync_db WHERE sync_db.dev_id = dev.id AND sync_db.purpose = "manualsync" OR sync_db.purpose = "autosync") AS count_items_dev'
 				)
 				->join('dev', 'sync_db.dev_id', '=', 'dev.id')
 				->groupBy('sync_db.dev_id')
