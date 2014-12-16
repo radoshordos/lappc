9d374a734523ea5e3a2b3f927c7a8b14daf4df0d
Radek Horáček <radoshordos@gmail.com>
2014-12-16 23:16:15 +0100

    half autoAllRecord dev

--- app/Authority/Runner/Task/Events/MixtureOnlyOneDev.php
+++ app/Authority/Runner/Task/Events/MixtureOnlyOneDev.php
@@ -8,15 +8,36 @@ use Authority\Runner\Task\TaskMessage;
 class MixtureOnlyOneDev extends TaskMessage implements iRun
 {
 
-    public function __construct($db)
-    {
-        parent::__construct($db);
-        $this->run();
-    }
+	public function __construct($db)
+	{
+		parent::__construct($db);
+		$this->run();
+	}
 
-    public function run()
-    {
-        $results = \DB::select('SELECT id,name
+	public function run() {
+		$this->autoAllRecord();
+		$this->autoSimpleRecord();
+	}
+
+	private function autoAllRecord()
+	{
+		$count_all = MixtureDev::where('purpose', '=', 'autoall')->count();
+		if ($count_all == 0) {
+			MixtureDev::create(['purpose' => 'autoall', 'name' => 'Všichni výrobci']);
+		}
+/*
+		$res = MixtureDevM2nDev::select('mixture_dev.dev_id')
+			->rightJoin('mixture_dev','mixture_dev.id','=','mixture_dev_m2n_dev.mixture_dev_id')
+			->whereNotIn('mixture_dev.purpose','=','autoall')
+			->lists(['mixture_dev.dev_id']);
+
+		var_dump($res);
+*/
+	}
+
+	private function autoSimpleRecord()
+	{
+		$results = \DB::select('SELECT id,name
                                 FROM dev
                                 WHERE id NOT IN (
                                     SELECT dev_id
@@ -30,24 +51,22 @@ class MixtureOnlyOneDev extends TaskMessage implements iRun
 
         ');
 
-        if ($results) {
-            foreach ($results as $val) {
+		if ($results) {
+			foreach ($results as $val) {
+				\DB::transaction(function () use ($val) {
 
-                \DB::transaction(function () use ($val) {
+					$mdev = new MixtureDev;
+					$mdev->purpose = 'autosimple';
+					$mdev->name = 'ONLY ' . $val->name;
+					$mdev->desc = 'AutoGenerate only dev: ' . $val->name;
+					$mdev->save();
 
-                    $mdev = new MixtureDev;
-                    $mdev->purpose = 'autosimple';
-                    $mdev->name = 'ONLY ' . $val->name;
-                    $mdev->desc = 'AutoGenerate only dev: ' . $val->name;
-                    $mdev->save();
-
-                    $mdmd = new MixtureDevM2nDev;
-                    $mdmd->mixture_dev_id = $mdev->id;
-                    $mdmd->dev_id = $val->id;
-                    $mdmd->save();
-
-                });
-            }
-        }
-    }
+					$mdmd = new MixtureDevM2nDev;
+					$mdmd->mixture_dev_id = $mdev->id;
+					$mdmd->dev_id = $val->id;
+					$mdmd->save();
+				});
+			}
+		}
+	}
 }
