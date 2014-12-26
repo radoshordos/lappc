c827d6d811600464e48e87be36dbb3a3ad140dbb
Radek Horáček <radoshordos@gmail.com>
2014-12-27 00:30:03 +0100

    vcard addres

--- app/views/web/global/footer.blade.php
+++ app/views/web/global/footer.blade.php
@@ -2,10 +2,10 @@
     <div class="large-18 columns">
         <hr>
         <div class="row">
-            <div class="large-9 columns">
+            <div class="large-6 columns">
                 <p>© Guru nářadí</p>
             </div>
-            <div class="large-9 columns">
+            <div class="large-6 columns">
                 <ul class="inline-list right">
                     <li>
                         <a href="#">Link 1</a>
@@ -13,12 +13,14 @@
                     <li>
                         <a href="#">Link 2</a>
                     </li>
-                    <li>
-                        <a href="#">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#">Link 4</a>
-                    </li>
+                </ul>
+            </div>
+            <div class="large-6 columns text-right">
+                <ul class="vcard text-left">
+                    <li class="fn">Nářadí Doležalová s.r.o</li>
+                    <li class="street-address">Jiráskova 2222</li>
+                    <li><span class="locality">Benešov</span> <span class="zip">25601</span></li>
+                    <li class="email"><a href="#">obchod@naradi.guru</a></li>
                 </ul>
             </div>
         </div>
