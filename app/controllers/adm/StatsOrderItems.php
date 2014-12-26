9292fe284b45472453d75dad51b1740dcefcaaf8
Radek Horáček <radoshordos@gmail.com>
2014-12-26 15:07:09 +0100

    Zakoupené zboží final

--- app/controllers/adm/StatsOrderItems.php
+++ app/controllers/adm/StatsOrderItems.php
@@ -1,29 +1,42 @@
 <?php
 
 use Authority\Eloquent\BuyOrderDbItems;
+use Authority\Eloquent\MixtureDev;
+use Authority\Eloquent\MixtureDevM2nDev;
 use Authority\Tools\SB;
 
 class StatsOrderItems extends \BaseController
 {
 	public function index()
 	{
-/*
-		$sum_prod = $db->fetchAll($db->select()->from("log2prod2booking", ["count_prod" => "COUNT(*)"])
-			->joinLeft("items", Model_Zendb::ZEND_LOG2PROD2BOOKING_2_ITEMS, [])
-			->joinLeft("prod", Model_Zendb::ZEND_ITEMS_2_PROD, ["prod_name"])
-			->where("lpb_ti_create >= ?", strtotime($_GET["day-start"]))
-			->where("lpb_ti_create <= ?", strtotime($_GET["day-end"]))
-			->where((intval($_GET["dev_id"]) > 0) ? $db->quoteInto("prod_id_dev = ?", intval($_GET['dev_id'])) : "1=1")
-			->group(["lpb_id_prod"])
-			->order(["count_prod DESC", "prod_name ASC"])
-		);
-		*/
+		if (Input::has('submit-orderitems')) {
+			$bodi = BuyOrderDbItems::selectRaw("prod.name,COUNT(*) AS count_item")
+				->leftJoin('items', 'buy_order_db_items.item_id', '=', 'items.id')
+				->leftJoin('prod', 'items.prod_id', '=', 'prod.id')
+				->whereNotNull('prod.id')
+				->groupBy('buy_order_db_items.item_id')
+				->orderByRaw("count_item DESC, prod.name ASC");
+
+			if (!empty(Input::get('day_start'))) {
+				$bodi->where('buy_order_db_items.created_at', '>=', Input::get('day_start'));
+			}
+
+			if (!empty(Input::get('day_end'))) {
+				$bodi->where('buy_order_db_items.created_at', '<=', Input::get('day_end'));
+			}
+
+			if (intval(Input::get('select_mixture_dev')) > 0) {
+				$bodi->whereIn('prod.dev_id', MixtureDevM2nDev::where('mixture_dev_id', '=', intval(Input::get('select_mixture_dev')))->lists('id'));
+			}
+		}
 
 		return View::make('adm.stats.orderitems.index', [
-			'min_insert_month'   => BuyOrderDbItems::selectRaw('DATE_FORMAT(FROM_UNIXTIME(MIN("created_at")), "%e %b %Y") AS min_insert_month')->pluck('created_at'),
-			'max_insert_month'   => BuyOrderDbItems::selectRaw('DATE_FORMAT(FROM_UNIXTIME(MAX("created_at")), "%e %b %Y") AS max_insert_month')->pluck('created_at'),
-			'select_mixture_dev' => SB::option("SELECT * FROM mixture_dev ORDER BY name,id", ['id' => '->name - [Výrobců &#8721;:->trigger_column_count]'], true),
-			'choice_mixture_dev' => Input::get('select_mixture_dev')
+			'min_insert_month'   => BuyOrderDbItems::selectRaw('DATE_FORMAT(FROM_UNIXTIME(MIN("created_at")), "%Y-%m-%d") AS min_insert_month')->pluck('min_insert_month'),
+			'select_mixture_dev' => SB::optionEloqent(MixtureDev::whereIn('purpose', ['autosimple', 'devgroup'])->orderByRaw('name,id')->get(), ['id' => '->name - [Výrobců &#8721;:->trigger_column_count]'], true),
+			'choice_day_start'   => (empty(Input::get('day_start'))) ? NULL : Input::get('day_start'),
+			'choice_day_end'     => (empty(Input::get('day_end'))) ? date('Y-m-d') : Input::get('day_end'),
+			'choice_mixture_dev' => Input::get('select_mixture_dev'),
+			'buy_order_db_items' => (Input::has('submit-orderitems')) ? $bodi->get() : NULL
 		]);
 	}
 }
--- app/views/adm/stats/orderitems/index.blade.php
+++ app/views/adm/stats/orderitems/index.blade.php
@@ -23,20 +23,44 @@ Zakoupené zboží
         <table class="table">
             <thead>
                 <tr>
-                    <th class="col-sm-3">{{ Form::label('day-start', 'Od dne') }}</th>
-                    <th class="col-sm-3">{{ Form::label('day-end', 'Do dne') }}</th>
+                    <th class="col-sm-3">{{ Form::label('day_start', 'Od dne') }}</th>
+                    <th class="col-sm-3">{{ Form::label('day_end', 'Do dne') }}</th>
                     <th>{{ Form::label('select_mixture_dev', 'Výrobce') }}</th>
-                    <th>{{ Form::submit('Zobrazit',['name' => 'submit-month-record','class' => 'btn btn-success']) }}</th>
+                    <th>{{ Form::submit('Zobrazit',['name' => 'submit-orderitems','class' => 'btn btn-success']) }}</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>{{ Form::input('date', 'day-start', NULL, ['class' => 'form-control', 'min' => $min_insert_month]);  }}</td>
-                    <td>{{ Form::input('date', 'day-end', NULL, ['class' => 'form-control', 'max' => $max_insert_month]);  }}</td>
+                    <td>{{ Form::input('date', 'day_start', $choice_day_start, ['class' => 'form-control', 'required' => 'required', 'min' => $min_insert_month, 'max' => date('Y-m-d')]) }}</td>
+                    <td>{{ Form::input('date', 'day_end', $choice_day_end , ['class' => 'form-control','required' => 'required', 'min' => $min_insert_month, 'max' => date('Y-m-d') ]) }}</td>
                     <td colspan="2">{{ Form::select('select_mixture_dev',$select_mixture_dev, $choice_mixture_dev, ['class'=> 'form-control', 'id' => 'select_mixture_dev']) }}</td>
                 </tr>
             </tbody>
         </table>
     </form>
     {{ Form::close() }}
+
+    @if (count($buy_order_db_items)>0)
+    <table class="table table-condensed">
+        <thead>
+            <tr>
+                <th class="text-center">&#8721;</th>
+                <th class="text-center">Název</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <td class="text-right" colspan="2">Zobrazeno: <b>{{ count($buy_order_db_items); }}</b> záznamů</td>
+            </tr>
+        </tfoot>
+        <tbody>
+        <?php foreach ($buy_order_db_items as $bodi) { ?>
+            <tr>
+                <td class="text-center"><?= $bodi->count_item; ?></td>
+                <td><?= $bodi->name; ?></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    @endif
 @stop
