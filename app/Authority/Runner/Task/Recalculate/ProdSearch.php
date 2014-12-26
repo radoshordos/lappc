fcd1e91f551d0ae039e1517bd901232d215f6da5
Radek Horáček <radoshordos@gmail.com>
2014-12-26 20:15:10 +0100

    sort by sell product

--- app/Authority/Runner/Task/Recalculate/ProdSearch.php
+++ app/Authority/Runner/Task/Recalculate/ProdSearch.php
@@ -4,6 +4,7 @@ use Authority\Runner\Task\iRun;
 use Authority\Runner\Task\TaskMessage;
 use Authority\Eloquent\Prod;
 use Authority\Eloquent\Items;
+use Authority\Eloquent\BuyOrderDbItems;
 use Authority\Eloquent\ViewProd;
 use Authority\Mapper\ViewProdMapper;
 
@@ -17,9 +18,10 @@ class ProdSearch extends TaskMessage implements iRun
 
 	public function run()
 	{
-		$this->searchInAlias();
-		$this->searchInItemsCode();
+		$this->searchInSell();
 		$this->searchInPrice();
+		$this->searchInItemsCode();
+		$this->searchInAlias();
 	}
 
 	public function searchInAlias()
@@ -61,6 +63,25 @@ class ProdSearch extends TaskMessage implements iRun
 			}
 		}
 		$this->addMessage("Provedena aktualizace cen pro setřídění");
+	}
+
+	public function searchInSell()
+	{
+		$bodi = BuyOrderDbItems::selectRaw("prod.id AS prod_id, COUNT(prod.id) AS count_prod")
+			->leftJoin('items', 'buy_order_db_items.item_id', '=', 'items.id')
+			->leftJoin('prod', 'items.prod_id', '=', 'prod.id')
+			->whereNotNull('prod.id')
+			->groupBy('items.prod_id')
+			->get();
+
+		if (!empty($bodi)) {
+			foreach ($bodi as $row) {
+				$prod = Prod::find($row->prod_id);
+				$prod->search_sell = $row->count_prod;
+				$prod->save();
+			}
+		}
+		$this->addMessage("Provedena aktualizace prodaných produktů");
 	}
 
 	private function friendlyString($source)
--- app/Authority/Web/Query/AjaxTree.php
+++ app/Authority/Web/Query/AjaxTree.php
@@ -3,16 +3,16 @@
 class AjaxTree
 {
 
-    public function orderBySort($db, $sort = NULL)
-    {
-        if ($sort == 'expensive') {
-            return $db->orderBy('prod_search_price', 'DESC');
-        } else if ($sort == 'sell') {
-            return $db->orderBy('prod_search_price', 'ASC');
-        } else if ($sort == 'fresh') {
-            return $db->orderBy('prod_created_at', 'DESC');
-        } else {
-            return $db->orderBy('prod_search_price', 'ASC');
-        }
-    }
+	public function orderBySort($db, $sort = NULL)
+	{
+		if ($sort == 'expensive') {
+			return $db->orderBy('prod_search_price', 'DESC');
+		} else if ($sort == 'sell') {
+			return $db->orderBy('prod_search_sell', 'DESC');
+		} else if ($sort == 'fresh') {
+			return $db->orderBy('prod_created_at', 'DESC');
+		} else {
+			return $db->orderBy('prod_search_price', 'ASC');
+		}
+	}
 }
