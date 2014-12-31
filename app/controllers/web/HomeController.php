673539896aea045a83ca9d36572f3ac1efb4f0fb
Radek Horáček <radoshordos@gmail.com>
2014-12-31 13:38:50 +0100

    namespace home fix

--- app/controllers/web/HomeController.php
+++ app/controllers/web/HomeController.php
@@ -9,26 +9,27 @@ use Carbon\Carbon;
 class HomeController extends EshopController
 {
 
-    public function show()
-    {
-        if (Input::has('term')) {
-            $counter = RecordVisitorsLooking::where('find_at', '>', new Carbon('last hour'))->where('filter_find', '=', Input::get('term'))->orderBy('id')->count();
-            if ($counter == 0) {
-                $dt = new DateTime;
-                RecordVisitorsLooking::create([
-                    'find_at'     => $dt->format('Y-m-d H:i:s'),
-                    'filter_find' => Input::get('term'),
-                    'count_dev'   => 0,
-                    'count_prod'  => 0
-                ]);
-            }
-        }
+	public function show()
+	{
+		if (Input::has('term')) {
+			$counter = RecordVisitorsLooking::where('find_at', '>', new Carbon('last hour'))->where('filter_find', '=', Input::get('term'))->orderBy('id')->count();
+			if ($counter == 0) {
+				$dt = new DateTime;
+				RecordVisitorsLooking::create([
+					'find_at'     => $dt->format('Y-m-d H:i:s'),
+					'filter_find' => Input::get('term'),
+					'count_dev'   => 0,
+					'count_prod'  => 0
+				]);
+			}
+		}
 
-        return View::make('web.home', [
-            'view_tree_array'  => ViewTree::whereIn('tree_group_type', ['prodaction', 'prodlist'])->orderBy('tree_id')->get(),
-            'tree_group' => TreeGroup::where('type', '=', 'prodlist')->get(),
-            'term' => Input::get('term')
-        ]);
-    }
+		return View::make('web.home', [
+			'namespace'       => 'home',
+			'view_tree_array' => ViewTree::whereIn('tree_group_type', ['prodaction', 'prodlist'])->orderBy('tree_id')->get(),
+			'tree_group'      => TreeGroup::where('type', '=', 'prodlist')->get(),
+			'term'            => Input::get('term')
+		]);
+	}
 
 }
