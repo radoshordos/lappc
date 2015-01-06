a4872f39666514792913b5d0ea9d663992589bac
Radek Horáček <radoshordos@gmail.com>
2015-01-06 18:47:38 +0100

    fix item import problem

--- app/database/seeds/ItemsSeeder.php
+++ app/database/seeds/ItemsSeeder.php
@@ -3,28 +3,31 @@
 class ItemsSeeder extends Seeder
 {
 
-    public function run()
-    {
-        include "migration/items.php";
+	public function run()
+	{
+		$items = [];
+		include "migration/items.php";
+		DB::table('items')->delete();
+		$now = strtotime('now');
 
-        DB::table('items')->delete();
-        $now = strtotime('now');
-        foreach ($items as $row) {
+		foreach ($items as $row) {
 
 //            $count = DB::table('prod')->where("id", '=', $row['items_id_prod'])->count();
- //           if ($count > 0) {
-                DB::table('items')->insert([
-                    'id'              => $row['items_id'],
-                    'prod_id'         => $row['items_id_prod'],
-                    'visible'         => $row['items_is_visible'],
-                    'availability_id' => $row['items_id_availibility'],
-                    'diff_val1_id'    => $row['items_id_diff1'],
-                    'diff_val2_id'    => $row['items_id_diff2'],
-                    'code_prod'       => $row['items_code_product'],
-                    'created_at'      => date("Y-m-d H:i:s", $row['items_ti_create']),
-                    'updated_at'      => date("Y-m-d H:i:s", $now),
-                ]);
-   //         }
-        }
-    }
+			//           if ($count > 0) {
+
+			DB::table('items')->insert([
+				'id'              => $row['items_id'],
+				'prod_id'         => $row['items_id_prod'],
+				'visible'         => $row['items_is_visible'],
+				'availability_id' => $row['items_id_availibility'],
+				'diff_val1_id'    => $row['items_id_diff1'],
+				'diff_val2_id'    => $row['items_id_diff2'],
+				'code_prod'       => $row['items_code_product'],
+				'created_at'      => date("Y-m-d H:i:s", $row['items_ti_create']),
+				'updated_at'      => date("Y-m-d H:i:s", $now),
+			]);
+
+			//         }
+		}
+	}
 }
