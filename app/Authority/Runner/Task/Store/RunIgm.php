66c001af860c7404ea52c8caea0e93947d414796
radoshordos <radoshordos@gmail.com>
2015-01-26 10:44:17 +0100

    IGM dph Fix

--- app/Authority/Runner/Task/Store/RunIgm.php
+++ app/Authority/Runner/Task/Store/RunIgm.php
@@ -2,178 +2,182 @@
 
 class RunIgm extends AbstractRunDev implements iItem
 {
-	const ROOT = 'SHOP';
+    const ROOT = 'SHOP';
 
-	function __construct($shop_item, $record_id)
-	{
-		parent::__construct($shop_item, $record_id);
-	}
+    function __construct($shop_item, $record_id)
+    {
+        parent::__construct($shop_item, $record_id);
+    }
 
-	private function analyseIdDev($dev_name)
-	{
-		switch ($dev_name) {
-			case 'IGM Basic Line' :
-			case 'IGM Expert Line' :
-			case 'IGM Professional Tools' :
-			case 'IGM Profi Line' :
-			case 'BASIC Line' :
-			case 'EXPERT Line' :
-			case 'IGM Professional' :
-			case 'PROFI Line' :
-				return 800;
-			case 'CMT Orange Tools' :
-				return 802;
-			case 'IGM Fachmann Tools' :
-				return 804;
-			case 'IGM Fachmann' :
-				return 804;
-			case 'JET' :
-				return 806;
-			case 'Silky' :
-				return 808;
-			case 'Titebond' :
-				return 810;
-			case 'Uvex' :
-				return 812;
-			default :
-				return 0;
-		}
-	}
+    private function analyseIdDev($dev_name)
+    {
+        switch ($dev_name) {
+            case 'IGM Basic Line' :
+            case 'IGM Expert Line' :
+            case 'IGM Professional Tools' :
+            case 'IGM Profi Line' :
+            case 'BASIC Line' :
+            case 'EXPERT Line' :
+            case 'IGM Professional' :
+            case 'PROFI Line' :
+                return 800;
+            case 'CMT Orange Tools' :
+                return 802;
+            case 'IGM Fachmann Tools' :
+                return 804;
+            case 'IGM Fachmann' :
+                return 804;
+            case 'JET' :
+                return 806;
+            case 'Silky' :
+                return 808;
+            case 'Titebond' :
+                return 810;
+            case 'Uvex' :
+                return 812;
+            default :
+                return 0;
+        }
+    }
 
-	private function analyseName($dev_name)
-	{
-		switch ($dev_name) {
-			case 'IGM Basic Line' :
-			case 'IGM Expert Line' :
-			case 'IGM Professional Tools' :
-			case 'IGM Profi Line' :
-			case 'BASIC Line' :
-			case 'EXPERT Line' :
-			case 'IGM Professional' :
-			case 'PROFI Line' :
-				return "IGM";
-			case 'CMT Orange Tools' :
-				return "CMT";
-			case 'IGM Fachmann Tools' :
-				return "CMT";
-			case 'IGM Fachmann' :
-				return 'Fachmann';
-			case 'JET' :
-				return 'JET';
-			case 'Silky' :
-				return 'Silky';
-			case 'Titebond' :
-				return 'Titebond';
-			case 'Uvex' :
-				return 'Uvex';
-			default :
-				return 'ERR_IGM';
-		}
-	}
+    private function analyseName($dev_name)
+    {
+        switch ($dev_name) {
+            case 'IGM Basic Line' :
+            case 'IGM Expert Line' :
+            case 'IGM Professional Tools' :
+            case 'IGM Profi Line' :
+            case 'BASIC Line' :
+            case 'EXPERT Line' :
+            case 'IGM Professional' :
+            case 'PROFI Line' :
+                return "IGM";
+            case 'CMT Orange Tools' :
+                return "CMT";
+            case 'IGM Fachmann Tools' :
+                return "CMT";
+            case 'IGM Fachmann' :
+                return 'Fachmann';
+            case 'JET' :
+                return 'JET';
+            case 'Silky' :
+                return 'Silky';
+            case 'Titebond' :
+                return 'Titebond';
+            case 'Uvex' :
+                return 'Uvex';
+            default :
+                return 'ERR_IGM';
+        }
+    }
 
-	public function setSyncIdDev()
-	{
-		$this->syncIdDev = $this->analyseIdDev($this->shopItem['MANUFACTURER']);
-	}
+    public function setSyncIdDev()
+    {
+        $this->syncIdDev = $this->analyseIdDev($this->shopItem['MANUFACTURER']);
+    }
 
-	function setSyncProdName()
-	{
-		if (isset($this->shopItem['PRODUCTNO']) && isset($this->shopItem['MANUFACTURER'])) {
-			$this->syncProdName = $this->analyseName($this->shopItem['MANUFACTURER']) . " " . trim($this->shopItem['PRODUCTNO']);
-		}
-	}
+    function setSyncProdName()
+    {
+        if (isset($this->shopItem['PRODUCTNO']) && isset($this->shopItem['MANUFACTURER'])) {
+            $this->syncProdName = $this->analyseName($this->shopItem['MANUFACTURER']) . " " . trim($this->shopItem['PRODUCTNO']);
+        }
+    }
 
-	public function setSyncProdDesc()
-	{
-		if (isset($this->shopItem['PRODUCTNAME'])) {
-			$this->syncProdDesc = (string)trim($this->shopItem['PRODUCTNAME']);
-		}
-	}
+    public function setSyncProdDesc()
+    {
+        if (isset($this->shopItem['PRODUCTNAME'])) {
+            $this->syncProdDesc = (string)trim($this->shopItem['PRODUCTNAME']);
+        }
+    }
 
-	function setSyncItemsCodeProduct()
-	{
-		if (isset($this->shopItem['PRODUCTNO'])) {
-			$this->syncItemsCodeProduct = (string)trim($this->shopItem['PRODUCTNO']);
-		}
-	}
+    function setSyncItemsCodeProduct()
+    {
+        if (isset($this->shopItem['PRODUCTNO'])) {
+            $this->syncItemsCodeProduct = (string)trim($this->shopItem['PRODUCTNO']);
+        }
+    }
 
-	public function setSyncItemsCodeEan()
-	{
-		if (isset($this->shopItem['EAN'])) {
-			$this->syncItemsCodeEan = (string)trim($this->shopItem['EAN']);
-		}
-	}
+    public function setSyncItemsCodeEan()
+    {
+        if (isset($this->shopItem['EAN'])) {
+            $this->syncItemsCodeEan = (string)trim($this->shopItem['EAN']);
+        }
+    }
 
-	public function setSyncItemsPriceStandard()
-	{
-		(!empty($this->shopItem['PRODUCTPRICE']) ? $this->syncItemsPriceStandard = doubleval($this->shopItem['PRODUCTPRICE']) : NULL);
-	}
+    public function setSyncItemsPriceStandard()
+    {
+        if (isset($this->shopItem['PRODUCTPRICE'])) {
+            $this->syncItemsPriceStandard = round(doubleval($this->shopItem['PRODUCTPRICE'] * self::DPH));
+        }
+    }
 
-	function setSyncItemsPriceAction()
-	{
-		(!empty($this->shopItem['PRODUCTACTIONPRICE']) ? $this->syncItemsPriceAction = doubleval($this->shopItem['PRODUCTACTIONPRICE']) : NULL);
-	}
+    function setSyncItemsPriceAction()
+    {
+        if (isset($this->shopItem['PRODUCTACTIONPRICE'])) {
+            $this->syncItemsPriceAction = round(doubleval($this->shopItem['PRODUCTACTIONPRICE'] * self::DPH));
+        }
+    }
 
-	function setSyncProdWeight()
-	{
-		if (isset($this->shopItem['MAINUNITWEIGHT'])) {
-			$this->syncProdWeight = round(floatval(str_replace(",", ".", $this->shopItem['MAINUNITWEIGHT'])), 2);
-		}
-	}
+    function setSyncProdWeight()
+    {
+        if (isset($this->shopItem['MAINUNITWEIGHT'])) {
+            $this->syncProdWeight = round(floatval(str_replace(",", ".", $this->shopItem['MAINUNITWEIGHT'])), 2);
+        }
+    }
 
-	public function setSyncCategoryText()
-	{
-		(!empty($this->shopItem['CATEGORY']) ? $this->syncCategoryText = $this->shopItem['CATEGORY'] : NULL);
-	}
+    public function setSyncCategoryText()
+    {
+        (!empty($this->shopItem['CATEGORY']) ? $this->syncCategoryText = $this->shopItem['CATEGORY'] : NULL);
+    }
 
-	function setSyncProdImgSourceArray()
-	{
-		if (!empty($this->shopItem['PRODUCTPICTURES'])) {
-			$ai = new \ArrayIterator();
-			$pp = (array)$this->shopItem['PRODUCTPICTURES'];
+    function setSyncProdImgSourceArray()
+    {
+        if (!empty($this->shopItem['PRODUCTPICTURES'])) {
+            $ai = new \ArrayIterator();
+            $pp = (array)$this->shopItem['PRODUCTPICTURES'];
 
-			if (count($pp) > 0) {
-				foreach ($pp as $val) {
-					$ai->append($val);
-				}
-			}
-			return $this->syncProdImgSourceArray = $ai->getArrayCopy();
-		}
-	}
+            if (count($pp) > 0) {
+                foreach ($pp as $val) {
+                    $ai->append($val);
+                }
+            }
+            return $this->syncProdImgSourceArray = $ai->getArrayCopy();
+        }
+    }
 
-	function setSyncProdAccessorySourceArray()
-	{
-		if (!empty($this->shopItem['PRODUCTACCESSORIES'])) {
+    function setSyncProdAccessorySourceArray()
+    {
+        if (!empty($this->shopItem['PRODUCTACCESSORIES'])) {
 
-			$ai = new \ArrayIterator();
-			$pa = (array)$this->shopItem['PRODUCTACCESSORIES'];
+            $ai = new \ArrayIterator();
+            $pa = (array)$this->shopItem['PRODUCTACCESSORIES'];
 
-			if (count($pa) > 0) {
-				foreach ($pa as $val) {
-					$ai->append($val);
-				}
-			}
-			return $this->syncProdAccessorySourceArray = $ai->getArrayCopy();
-		}
-	}
+            if (count($pa) > 0) {
+                foreach ($pa as $val) {
+                    $ai->append($val);
+                }
+            }
+            return $this->syncProdAccessorySourceArray = $ai->getArrayCopy();
+        }
+    }
 
-	function setSyncUrl()
-	{
-		// TODO: Implement setSyncUrl() method.
-	}
+    function setSyncUrl()
+    {
+        // TODO: Implement setSyncUrl() method.
+    }
 
-	function setSyncWarranty()
-	{
-		// TODO: Implement setSyncWarranty() method.
-	}
+    function setSyncWarranty()
+    {
+        // TODO: Implement setSyncWarranty() method.
+    }
 
-	function setSyncItemsAvailabilityCount()
-	{
-		// TODO: Implement setSyncItemsAvailabilityCount() method.
-	}
+    function setSyncItemsAvailabilityCount()
+    {
+        // TODO: Implement setSyncItemsAvailabilityCount() method.
+    }
 
-	function setSyncProdFileSourceArray()
-	{
-		// TODO: Implement setSyncProdFileSourceArray() method.
-	}
+    function setSyncProdFileSourceArray()
+    {
+        // TODO: Implement setSyncProdFileSourceArray() method.
+    }
 }
