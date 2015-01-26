d61e2bf3d8d3a7a3604aaf76ff4f9f7065400860
radoshordos <radoshordos@gmail.com>
2015-01-26 16:28:47 +0100

    sync_db more filters

--- app/controllers/adm/SyncDbController.php
+++ app/controllers/adm/SyncDbController.php
@@ -5,79 +5,125 @@ use Authority\Eloquent\SyncDb;
 
 class SyncDbController extends \BaseController
 {
-	public function index()
-	{
+    public function index()
+    {
 
-		$dev = \DB::table('mixture_dev_m2n_dev')->where('mixture_dev_id', Input::get('select_mixture_dev'))->lists('dev_id');
-		$select_mixture_dev = SB::option("SELECT * FROM mixture_dev WHERE purpose = 'autosimple' OR purpose = 'devgroup' ORDER BY name", ['id' => '->name'], true);
+        $dev = \DB::table('mixture_dev_m2n_dev')->where('mixture_dev_id', Input::get('select_mixture_dev'))->lists('dev_id');
+        $select_mixture_dev = SB::option("SELECT * FROM mixture_dev WHERE purpose = 'autosimple' OR purpose = 'devgroup' ORDER BY name", ['id' => '->name'], TRUE);
 
-		if (count($dev) > 0) {
-			$select_categorytext = SB::optionEloqent(SyncDb::select('categorytext')->whereIn('purpose', ['manualsync', 'autosync'])->whereIn('sync_db.dev_id', $dev)->distinct()->get(), ['categorytext' => '->categorytext'], true);
-		}
+        if (count($dev) > 0) {
+            $select_categorytext = SB::optionEloqent(SyncDb::select('categorytext')->whereIn('purpose', ['manualsync', 'autosync'])->whereIn('sync_db.dev_id', $dev)->distinct()->get(), ['categorytext' => '->categorytext'], TRUE);
+        }
 
-		if (strlen(Input::get('select_connect')) == 0 || count($dev) == 0) {
+        if (strlen(Input::get('select_connect')) == 0 || count($dev) == 0) {
 
-			return View::make('adm.sync.db.index', [
-				'input'               => Input::all(),
-				'select_mixture_dev'  => $select_mixture_dev,
-				'select_categorytext' => (isset($select_categorytext) ? $select_categorytext : [])
-			]);
+            return View::make('adm.sync.db.index', [
+                'input'               => Input::all(),
+                'select_mixture_dev'  => $select_mixture_dev,
+                'select_categorytext' => (isset($select_categorytext) ? $select_categorytext : [])
+            ]);
 
-		} else {
+        } else {
 
-			$db = \DB::table('sync_db');
+            $db = \DB::table('sync_db');
+            $db->whereIn('sync_db.dev_id', $dev);
 
-			switch (Input::get('select_connect')) {
+            switch (Input::get('select_connect')) {
 
-				case 'code_ean':
-					$db->leftJoin('items', "items.code_ean", '=', "sync_db.code_ean");
-					$db->leftJoin('prod', 'prod.id', '=', 'items.prod_id');
-					break;
-				case 'name':
-					$db->leftJoin('prod', 'prod.name', '=', 'sync_db.name');
-					$db->leftJoin('items', 'prod.id', '=', 'items.prod_id');
-					break;
-				default:
-					$db->leftJoin('items', "items.code_prod", '=', "sync_db.code_prod");
-					$db->leftJoin('prod', 'prod.id', '=', 'items.prod_id');
-					break;
-			}
+                case 'code_ean':
+                    $db->leftJoin('items', "items.code_ean", '=', "sync_db.code_ean");
+                    $db->leftJoin('prod', 'prod.id', '=', 'items.prod_id');
+                    break;
+                case 'name':
+                    $db->leftJoin('prod', 'prod.name', '=', 'sync_db.name');
+                    $db->leftJoin('items', 'prod.id', '=', 'items.prod_id');
+                    break;
+                default:
+                    $db->leftJoin('items', "items.code_prod", '=', "sync_db.code_prod");
+                    $db->leftJoin('prod', 'prod.id', '=', 'items.prod_id');
+                    break;
+            }
 
-			switch (Input::get('join')) {
-				case 'left':
-					$db->whereNotNull('prod.name');
-					break;
-				default:
-					break;
-			}
+            switch (Input::get('join')) {
+                case 'left':
+                    $db->whereNotNull('prod.name');
+                    break;
+                case 'noleft':
+                    $db->whereNull('prod.name');
+                    break;
+                default:
+                    break;
+            }
 
-			if (Input::has('categorytext')) {
-				$db->where('categorytext', '=', Input::get('categorytext'));
-			}
+            if (Input::has('categorytext')) {
+                $db->where('categorytext', '=', Input::get('categorytext'));
+            }
 
-			$db->select([
-				'sync_db.id AS sync_id',
-				'sync_db.code_ean AS sync_code_ean',
-				'sync_db.code_prod AS sync_code_prod',
-				'sync_db.name AS sync_name',
-				'sync_db.desc AS sync_desc',
-				'sync_db.price_standard AS sync_price_standard',
-				'items.code_ean AS items_code_ean',
-				'items.code_prod AS items_code_prod',
-				'prod.id AS prod_id',
-				'prod.price AS prod_price',
-				'prod.name AS prod_name',
-				'prod.tree_id AS prod_tree_id'
-			]);
+            switch (Input::get('money')) {
+                case '1':
+                    $db->where('price_standard', '=', 'price')->whereNotNull('prod.price');
+                    break;
+                case '2':
+                    $db->where('price_standard', '!=', 'price')->whereNotNull('prod.price');
+                    break;
+                default:
+                    break;
+            }
 
-			$db->whereIn('sync_db.dev_id', $dev);
+            switch (Input::get('accessory')) {
+                case '1':
+                    $db->where('count_accessory', '>', '0');
+                    break;
+                case '2':
+                    $db->where('count_accessory', '=', '0');
+                    break;
+                default:
+                    break;
+            }
 
-			return View::make('adm.sync.db.index', [
-				'db'                  => $db->paginate(intval(Input::get('limit') > 0) ? Input::get('limit') : 20),
-				'input'               => Input::all(),
-				'select_mixture_dev'  => $select_mixture_dev,
-				'select_categorytext' => (isset($select_categorytext) ? $select_categorytext : [])
-			]);
-		}
-	}
+            switch (Input::get('file')) {
+                case '1':
+                    $db->where('count_file', '>', '0');
+                    break;
+                case '2':
+                    $db->where('count_file', '=', '0');
+                    break;
+                default:
+                    break;
+            }
+
+            switch (Input::get('img')) {
+                case '1':
+                    $db->where('count_img', '>', '0');
+                    break;
+                case '2':
+                    $db->where('count_img', '=', '0');
+                    break;
+                default:
+                    break;
+            }
+
+            $db->select([
+                'sync_db.id AS sync_id',
+                'sync_db.code_ean AS sync_code_ean',
+                'sync_db.code_prod AS sync_code_prod',
+                'sync_db.name AS sync_name',
+                'sync_db.desc AS sync_desc',
+                'sync_db.price_standard AS sync_price_standard',
+                'items.code_ean AS items_code_ean',
+                'items.code_prod AS items_code_prod',
+                'prod.id AS prod_id',
+                'prod.price AS prod_price',
+                'prod.name AS prod_name',
+                'prod.tree_id AS prod_tree_id'
+            ]);
+
+            return View::make('adm.sync.db.index', [
+                'db'                  => $db->paginate(intval(Input::get('limit') > 0) ? Input::get('limit') : 20),
+                'input'               => Input::all(),
+                'select_mixture_dev'  => $select_mixture_dev,
+                'select_categorytext' => (isset($select_categorytext) ? $select_categorytext : [])
+            ]);
+        }
+    }
 }
--- app/views/adm/sync/db/index.blade.php
+++ app/views/adm/sync/db/index.blade.php
@@ -18,9 +18,36 @@ Synchronizační databáze
               checkboxes.prop('checked', false);
             }
         });
-        $("#select_mixture_dev").select2({});
-        $("#select_connect").select2({});
+        $("#select_mixture_dev").select2({
+            placeholder: "Seskupení výrobců",
+            allowClear: true
+        });
+        $("#select_connect").select2({
+            placeholder: "Propojit podle",
+            allowClear: true
+        });
         $("#join").select2({});
+        $("#limit").select2({});
+        $("#accessory").select2({
+            placeholder: "Má příslušenství",
+            allowClear: true
+        });
+        $("#file").select2({
+            placeholder: "Má přílohu",
+            allowClear: true
+        });
+        $("#img").select2({
+            placeholder: "Má obrázek",
+            allowClear: true
+        });
+        $("#categorytext").select2({
+            placeholder: "Kategirie produktů",
+            allowClear: true
+        });
+        $("#money").select2({
+            placeholder: "Cena Sync => DB",
+            allowClear: true
+        });
     });
 </script>
 @stop
@@ -31,21 +58,35 @@ Synchronizační databáze
     <form>
         <div class="row">
             <div class="col-xs-3">
-                {{ Form::select('select_connect', ['' => 'Propojit podle', 'code_prod' => 'Kódu produktu', 'code_ean' => 'EAN produktu', 'name' => 'Názvu produktu'], (isset($input['select_connect']) ? $input['select_connect'] : NULL), ['id'=> 'select_connect', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
+                {{ Form::select('select_connect', ['' => '', 'code_prod' => 'Kódu produktu', 'code_ean' => 'EAN produktu', 'name' => 'Názvu produktu'], (isset($input['select_connect']) ? $input['select_connect'] : NULL), ['id'=> 'select_connect', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
             </div>
             <div class="col-xs-4">
                 {{ Form::select('select_mixture_dev', $select_mixture_dev, (isset($input['select_mixture_dev']) ? $input['select_mixture_dev'] : NULL), ['id'=> 'select_mixture_dev', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
             </div>
             <div class="col-xs-3">
-                {{ Form::select('join', ['left' => 'Spárované','inner' => 'Všechny'], (isset($input['join']) ? $input['join'] : NULL), ['id'=> 'join', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
+                {{ Form::select('join', ['left' => 'Spárované','noleft' => 'Nespárované','inner' => 'Všechny'], (isset($input['join']) ? $input['join'] : NULL), ['id'=> 'join', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
             </div>
             <div class="col-xs-2">
                 {{ Form::select('limit', ['20' => 'Limit 20','80' => 'Limit 80','500' => 'Limit 500'], (isset($input['limit']) ? $input['limit'] : NULL), ['id'=> 'limit', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
             </div>
         </div>
         <div class="row" style="margin-top: .4em">
-            <div class="col-xs-12">
+            <div class="col-xs-10">
                 {{ Form::select('categorytext', $select_categorytext, (isset($input['categorytext']) ? $input['categorytext'] : NULL), ['id'=> 'categorytext', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
+            </div>
+            <div class="col-xs-2">
+                {{ Form::select('money', ['' => '','1' => 'Rozdílná','2' => 'Stejná'], (isset($input['money']) ? $input['money'] : NULL), ['id'=> 'money', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
+            </div>
+        </div>
+        <div class="row" style="margin-top: .4em">
+            <div class="col-xs-4">
+                {{ Form::select('accessory', ['' => '','1' => 'ANO','2' => 'NE'], (isset($input['accessory']) ? $input['accessory'] : NULL), ['id'=> 'accessory', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
+            </div>
+            <div class="col-xs-4">
+                {{ Form::select('file', ['' => '','1' => 'ANO','2' => 'NE'], (isset($input['file']) ? $input['file'] : NULL), ['id'=> 'file', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
+            </div>
+            <div class="col-xs-4">
+                {{ Form::select('img', ['' => '','1' => 'ANO','2' => 'NE'], (isset($input['img']) ? $input['img'] : NULL), ['id'=> 'img', 'class'=> 'form-control', 'onchange' => 'this.form.submit()']) }}
             </div>
         </div>
     </form>
@@ -57,11 +98,11 @@ Synchronizační databáze
     <thead>
         <tr>
             <th>{{ Form::checkbox('select_all', NULL ,NULL, ['id'=>'select_all']) }}</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <th>Kód</th>
+            <th>EAN</th>
+            <th>Název</th>
+            <th>Popis</th>
+            <th class="text-right">Cena</th>
         </tr>
     </thead>
     <tbody>
@@ -72,7 +113,7 @@ Synchronizační databáze
         <td>{{ $row->sync_code_ean }}</td>
         <td>{{ $row->sync_name }}</td>
         <td>{{ $row->sync_desc }}</td>
-        <td>{{ $row->sync_price_standard }}</td>
+        <td class="{{ ($row->sync_price_standard > $row->prod_price ? 'text-right bg-danger' : "text-right") }}">{{ round($row->sync_price_standard) }}</td>
     </tr>
     @if ($row->prod_name)
     <tr class="bg-info">
@@ -81,7 +122,7 @@ Synchronizační databáze
         <td>{{ $row->items_code_ean }}</td>
         <td><a href="{{ URL::route('adm.product.prod.edit',[$row->prod_tree_id, $row->prod_id]); }}">{{ $row->prod_name }}</a></td>
         <td></td>
-        <td>{{ $row->prod_price }}</td>
+        <td class="{{ ($row->sync_price_standard < $row->prod_price ? 'text-right bg-danger' : "text-right") }}">{{ round($row->prod_price) }}</td>
     </tr>
     @endif
     @endforeach
