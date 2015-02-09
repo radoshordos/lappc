05ea9447d9975459da5711abc0bd51fe6871c342
Radek Horáček <radoshordos@gmail.com>
2015-02-09 20:45:26 +0100

    fix name sync

--- app/database/migrations/2014_06_30_140000_sync_template_m2n_column.php
+++ app/database/migrations/2014_06_30_140000_sync_template_m2n_column.php
@@ -3,12 +3,12 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SyncTemplateM2nColmun extends Migration
+class SyncTemplateM2nColumn extends Migration
 {
 
     public function up()
     {
-        Schema::create('sync_template_m2n_colmun', function (Blueprint $table) {
+        Schema::create('sync_template_m2n_column', function (Blueprint $table) {
 
             $table->increments('id');
             $table->integer('template_id')->unsigned();
@@ -20,48 +20,48 @@ class SyncTemplateM2nColmun extends Migration
             $table->foreign('column_id')->references('id')->on('sync_csv_column')->onUpdate('cascade')->onDelete('cascade');
         });
 
-        DB::unprepared('DROP TRIGGER IF EXISTS sync_template_m2n_colmun_ai');
+        DB::unprepared('DROP TRIGGER IF EXISTS sync_template_m2n_column_ai');
         DB::unprepared('
-            CREATE TRIGGER sync_template_m2n_colmun_ai
-            AFTER INSERT ON sync_template_m2n_colmun FOR EACH ROW
+            CREATE TRIGGER sync_template_m2n_column_ai
+            AFTER INSERT ON sync_template_m2n_column FOR EACH ROW
             BEGIN
                 UPDATE sync_csv_template
                 SET trigger_column_count =
                     (
                         SELECT COUNT(*)
-                        FROM sync_template_m2n_colmun
+                        FROM sync_template_m2n_column
                         WHERE template_id = NEW.template_id
                     )
                 WHERE sync_csv_template.id = NEW.template_id;
             END
         ');
 
-        DB::unprepared('DROP TRIGGER IF EXISTS sync_template_m2n_colmun_au');
+        DB::unprepared('DROP TRIGGER IF EXISTS sync_template_m2n_column_au');
         DB::unprepared('
-            CREATE TRIGGER sync_template_m2n_colmun_au
-            AFTER UPDATE ON sync_template_m2n_colmun FOR EACH ROW
+            CREATE TRIGGER sync_template_m2n_column_au
+            AFTER UPDATE ON sync_template_m2n_column FOR EACH ROW
             BEGIN
                 UPDATE sync_csv_template
                 SET trigger_column_count =
                     (
                         SELECT COUNT(*)
-                        FROM sync_template_m2n_colmun
+                        FROM sync_template_m2n_column
                         WHERE template_id = NEW.template_id
                     )
                 WHERE sync_csv_template.id = NEW.template_id;
             END
         ');
 
-        DB::unprepared('DROP TRIGGER IF EXISTS sync_template_m2n_colmun_ad');
+        DB::unprepared('DROP TRIGGER IF EXISTS sync_template_m2n_column_ad');
         DB::unprepared('
-            CREATE TRIGGER sync_template_m2n_colmun_ad
-            AFTER DELETE ON sync_template_m2n_colmun FOR EACH ROW
+            CREATE TRIGGER sync_template_m2n_column_ad
+            AFTER DELETE ON sync_template_m2n_column FOR EACH ROW
             BEGIN
                 UPDATE sync_csv_template
                 SET trigger_column_count =
                     (
                         SELECT COUNT(*)
-                        FROM sync_template_m2n_colmun
+                        FROM sync_template_m2n_column
                         WHERE template_id = OLD.template_id
                     )
                 WHERE sync_csv_template.id = OLD.template_id;
@@ -71,7 +71,7 @@ class SyncTemplateM2nColmun extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('sync_template_m2n_colmun');
+        Schema::dropIfExists('sync_template_m2n_column');
     }
 
 }
