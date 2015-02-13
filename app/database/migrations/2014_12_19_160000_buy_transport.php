f38422721b86379ea2c4e5b10463946b9c23d2c1
Radek Horáček <radoshordos@gmail.com>
2015-02-13 20:07:41 +0100

    buy transport more

--- app/database/migrations/2014_12_19_160000_buy_transport.php
+++ app/database/migrations/2014_12_19_160000_buy_transport.php
@@ -12,13 +12,14 @@ class BuyTransport extends Migration
 			$table->tinyInteger('id')->unsigned()->primary();
 			$table->tinyInteger('transport_type_id')->unsigned();
 			$table->boolean('active')->default(1);
-			$table->string('alias',32)->uniqie();
+			$table->string('alias', 32)->uniqie();
 			$table->string('name', '64');
 			$table->string('desc', '96')->unique();
 			$table->double('price_start')->unsigned();
 			$table->double('price_end')->unsigned();
-			$table->decimal('weight_start', 8, 3)->unsigned();
-			$table->decimal('weight_end', 8, 3)->unsigned();
+			$table->double('weight_start')->unsigned();
+			$table->double('weight_end')->unsigned();
+			$table->double('price_transport')->unsigned();
 
 			$table->engine = 'InnoDB';
 			$table->foreign('transport_type_id')->references('id')->on('buy_transport_type')->onUpdate('cascade')->onDelete('no action');
@@ -29,5 +30,4 @@ class BuyTransport extends Migration
 	{
 		Schema::dropIfExists('buy_transport');
 	}
-
 }
--- app/database/seeds/BuyTransportSeeder.php
+++ app/database/seeds/BuyTransportSeeder.php
@@ -16,7 +16,8 @@ class BuyTransportSeeder extends Seeder
 			'price_start'       => 0,
 			'price_end'         => 10000,
 			'weight_start'      => 0,
-			'weight_end'        => 10
+			'weight_end'        => 10,
+			'price_transport'   => 30
 		]);
 
 		DB::table('buy_transport')->insert([
@@ -28,31 +29,34 @@ class BuyTransportSeeder extends Seeder
 			'price_start'       => 0,
 			'price_end'         => 999999999,
 			'weight_start'      => 0,
-			'weight_end'        => 999999999
+			'weight_end'        => 999999999,
+			'price_transport'   => 0
 		]);
 
 		DB::table('buy_transport')->insert([
 			'id'                => $i++,
-			'alias'             => 'balik',
+			'alias'             => 'balik-do-30',
 			'transport_type_id' => 3,
-			'name'              => 'Balík',
+			'name'              => 'Balík do 30kg',
 			'desc'              => 'Balík | 0-10000 | 0-30',
 			'price_start'       => 0,
 			'price_end'         => 10000,
 			'weight_start'      => 0,
-			'weight_end'        => 30
+			'weight_end'        => 30,
+			'price_transport'   => 100
 		]);
 
 		DB::table('buy_transport')->insert([
 			'id'                => $i++,
-			'alias'             => 'balik',
+			'alias'             => 'balik-30-plus',
 			'transport_type_id' => 3,
-			'name'              => 'Balík',
+			'name'              => 'Balík 30kg+',
 			'desc'              => 'Balík | 0-10000 | 30-100',
 			'price_start'       => 0,
 			'price_end'         => 10000,
 			'weight_start'      => 30.001,
-			'weight_end'        => 100
+			'weight_end'        => 100,
+			'price_transport'   => 200
 		]);
 
 		DB::table('buy_transport')->insert([
@@ -64,7 +68,8 @@ class BuyTransportSeeder extends Seeder
 			'price_start'       => 0,
 			'price_end'         => 50000,
 			'weight_start'      => 0,
-			'weight_end'        => 30
+			'weight_end'        => 30,
+			'price_transport'   => 130
 		]);
 	}
 }
