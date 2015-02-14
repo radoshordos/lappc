f0005f141c332bce5d06c0193560d94ddcf668bd
Radek Horáček <radoshordos@gmail.com>
2015-02-14 22:57:22 +0100

    transport

--- app/database/seeds/BuyTransportSeeder.php
+++ app/database/seeds/BuyTransportSeeder.php
@@ -9,10 +9,49 @@ class BuyTransportSeeder extends Seeder
 
 		DB::table('buy_transport')->insert([
 			'id'                => $i++,
+			'alias'             => 'gls-0kg-30kg',
+			'transport_type_id' => 3,
+			'name'              => 'Kurýrní službou GLS',
+			'desc'              => 'Kurýrní službou GLS | 0-50000 | 0-30',
+			'price_start'       => 0,
+			'price_end'         => 50000,
+			'weight_start'      => 0,
+			'weight_end'        => 30,
+			'price_transport'   => 130
+		]);
+
+		DB::table('buy_transport')->insert([
+			'id'                => $i++,
+			'alias'             => 'toptrans-30kg-100kg',
+			'transport_type_id' => 3,
+			'name'              => 'Expresní doprava TOPTRANS',
+			'desc'              => 'Expresní doprava TOPTRANS | 0-50000 | 30-100',
+			'price_start'       => 0,
+			'price_end'         => 50000,
+			'weight_start'      => 30,
+			'weight_end'        => 100,
+			'price_transport'   => 250
+		]);
+
+		DB::table('buy_transport')->insert([
+			'id'                => $i++,
+			'alias'             => 'dobirka',
+			'transport_type_id' => 3,
+			'name'              => 'Dobírka',
+			'desc'              => 'Dobírka | 0-50000 | 0-30',
+			'price_start'       => 0,
+			'price_end'         => 50000,
+			'weight_start'      => 0,
+			'weight_end'        => 30,
+			'price_transport'   => 130
+		]);
+
+		DB::table('buy_transport')->insert([
+			'id'                => $i++,
 			'alias'             => 'zasilkovna',
 			'transport_type_id' => 5,
-			'name'              => 'Osobní odběr - Zásilkovna',
-			'desc'              => 'Osobní odběr - Zásilkovna | 0-10000 | 0-10',
+			'name'              => 'Zásilkovna',
+			'desc'              => 'Zásilkovna | 0-10000 | 0-10',
 			'price_start'       => 0,
 			'price_end'         => 10000,
 			'weight_start'      => 0,
@@ -31,45 +70,6 @@ class BuyTransportSeeder extends Seeder
 			'weight_start'      => 0,
 			'weight_end'        => 999999999,
 			'price_transport'   => 0
-		]);
-
-		DB::table('buy_transport')->insert([
-			'id'                => $i++,
-			'alias'             => 'balik-do-30',
-			'transport_type_id' => 3,
-			'name'              => 'Balík do 30kg',
-			'desc'              => 'Balík | 0-10000 | 0-30',
-			'price_start'       => 0,
-			'price_end'         => 10000,
-			'weight_start'      => 0,
-			'weight_end'        => 30,
-			'price_transport'   => 100
-		]);
-
-		DB::table('buy_transport')->insert([
-			'id'                => $i++,
-			'alias'             => 'balik-30-plus',
-			'transport_type_id' => 3,
-			'name'              => 'Balík 30kg+',
-			'desc'              => 'Balík | 0-10000 | 30-100',
-			'price_start'       => 0,
-			'price_end'         => 10000,
-			'weight_start'      => 30,
-			'weight_end'        => 100,
-			'price_transport'   => 200
-		]);
-
-		DB::table('buy_transport')->insert([
-			'id'                => $i++,
-			'alias'             => 'dobirka',
-			'transport_type_id' => 3,
-			'name'              => 'Dobírka',
-			'desc'              => 'Dobírka | 0-50000 | 0-30',
-			'price_start'       => 0,
-			'price_end'         => 50000,
-			'weight_start'      => 0,
-			'weight_end'        => 30,
-			'price_transport'   => 130
 		]);
 	}
 }
