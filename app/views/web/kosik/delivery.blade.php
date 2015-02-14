173497e157182560e5c7204050d9c26811dafa97
Radek Horáček <radoshordos@gmail.com>
2015-02-14 13:24:38 +0100

    transport and payment

--- app/views/web/kosik/delivery.blade.php
+++ app/views/web/kosik/delivery.blade.php
@@ -1,15 +1,16 @@
 <fieldset>
-    <legend>TRANSPORT</legend>
+    <legend>Zvolte způsob doručení</legend>
     @if (!empty($buy_transport))
         @foreach($buy_transport as $bt)
             <div class="row">
                 <div class="large-17 columns small-centered">
                     @if (!empty($customer) && $bt->id == $customer->delivery_id)
-                        <input id="{{ $bt->alias }}" type="radio" required="required" autocomplete="off" name="delivery_id" value="{{ $bt->id }}" checked="checked">
+                        <div class="small-1 columns"><input id="{{ $bt->alias }}" type="radio" required="required" autocomplete="off" name="delivery_id" value="{{ $bt->id }}" checked="checked"></div>
                     @else
-                        <input id="{{ $bt->alias }}" type="radio" required="required" autocomplete="off" name="delivery_id" value="{{ $bt->id }}">
+                        <div class="small-1 columns"><input id="{{ $bt->alias }}" type="radio" required="required" autocomplete="off" name="delivery_id" value="{{ $bt->id }}"></div>
                     @endif
-                    <label for="{{ $bt->alias }}">{{ $bt->name }}</label>
+                    <div class="small-9 columns"><label for="{{ $bt->alias }}">{{ $bt->name }}</label></div>
+                    <div class="small-3 columns"><strong>{{ ($bt->price_transport == 0 ? "ZDARMA" : $bt->price_transport." Kč") }}</strong></div>
                 </div>
             </div>
         @endforeach
--- app/views/web/kosik/payment.blade.php
+++ app/views/web/kosik/payment.blade.php
@@ -1,18 +1,16 @@
 <fieldset>
-    <legend>PLATBA</legend>
+    <legend>Zvolte způsob platby</legend>
     @if (!empty($buy_payment))
         @foreach($buy_payment as $bp)
             <div class="row">
                 <div class="large-17 columns small-centered">
-                    <div class="row">
-                        @if (!empty($customer) && $bp->id == $customer->payment_id)
-                            <div class="small-1 columns"><input id="{{ $bp->alias }}" type="radio" required="required" autocomplete="off" name="payment_id" value="{{ $bp->id }}" checked="checked"></div>
-                        @else
-                            <div class="small-1 columns"><input id="{{ $bp->alias }}" type="radio" required="required" autocomplete="off" name="payment_id" value="{{ $bp->id }}"></div>
-                        @endif
-                        <div class="small-8 columns"><label for="{{ $bp->alias }}">{{ $bp->name }}</label></div>
-                        <div class="small-4 columns">ZDARMA</div>
-                    </div>
+                    @if (!empty($customer) && $bp->id == $customer->payment_id)
+                        <div class="small-1 columns"><input id="{{ $bp->alias }}" type="radio" required="required" autocomplete="off" name="payment_id" value="{{ $bp->id }}" checked="checked"></div>
+                    @else
+                        <div class="small-1 columns"><input id="{{ $bp->alias }}" type="radio" required="required" autocomplete="off" name="payment_id" value="{{ $bp->id }}"></div>
+                    @endif
+                    <div class="small-8 columns"><label for="{{ $bp->alias }}">{{ $bp->name }}</label></div>
+                    <div class="small-4 columns"><strong>{{ ($bp->price_payment == 0 ? "ZDARMA" : $bp->price_payment." Kč") }}</strong></div>
                 </div>
             </div>
         @endforeach
