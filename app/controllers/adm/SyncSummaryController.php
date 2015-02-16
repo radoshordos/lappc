fb8ed61aaf33da09d4614851c2a8cb56922f65d9
Radek Horáček <radoshordos@gmail.com>
2015-02-16 23:19:12 +0100

    summary sync start

--- app/controllers/adm/SyncSummaryController.php
+++ app/controllers/adm/SyncSummaryController.php
@@ -8,7 +8,8 @@ class SyncSummaryController extends \BaseController
 	{
 		return View::make('adm.sync.summary.index', [
 			'summary' => SyncDb::selectRaw(
-					'dev.name as dev_name,
+					'dev.id as dev_id,
+					 dev.name as dev_name,
 					(SELECT COUNT(*) FROM sync_db WHERE sync_db.item_id IS NOT NULL AND sync_db.dev_id = dev.id AND sync_db.purpose = "manualsync" OR sync_db.purpose = "autosync") AS count_insert_prod,
 					(SELECT COUNT(*) FROM sync_db WHERE sync_db.dev_id = dev.id AND sync_db.purpose = "manualsync" OR sync_db.purpose = "autosync") AS count_items_dev'
 				)
--- app/views/adm/sync/summary/index.blade.php
+++ app/views/adm/sync/summary/index.blade.php
@@ -10,6 +10,7 @@ Přehled synchronizace výrobců
 @section('content')
     <table class="table">
         <thead>
+            <th>#ID</th>
             <th>Výrobce</th>
             <th>Zbývá vložit</th>
             <th>ALL SyncDB</th>
@@ -17,6 +18,7 @@ Přehled synchronizace výrobců
         <tbody>
         @foreach($summary as $row)
             <tr>
+                <td>{{ $row->dev_id }}</td>
                 <td>{{ $row->dev_name }}</td>
                 <td>{{ $row->count_items_dev - $row->count_insert_prod }}</td>
                 <td>{{ $row->count_items_dev }}</td>
