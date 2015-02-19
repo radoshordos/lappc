85de28e7a8006d10942dd251becdf80a664d6df8
Radek Horáček <radoshordos@gmail.com>
2015-02-19 23:01:05 +0100

    ProdDifferenceSeeder

--- app/database/seeds/ProdDifferenceSeeder.php
+++ app/database/seeds/ProdDifferenceSeeder.php
@@ -3,32 +3,34 @@
 class ProdDifferenceSeeder extends Seeder
 {
 
-    public function run()
-    {
-        DB::table('prod_difference')->delete();
+	public function run()
+	{
+		/* IN SHOP (OLD)
+		$prod2difference = [
+			['pd_id' => '1', 'pd_id_diffmode' => '1', 'pd_visible' => '1', 'pd_lenght' => '0', 'pd_name' => 'Základní'],
+			['pd_id' => '10', 'pd_id_diffmode' => '2', 'pd_visible' => '1', 'pd_lenght' => '1', 'pd_name' => 'Velikost'],
+			['pd_id' => '20', 'pd_id_diffmode' => '2', 'pd_visible' => '1', 'pd_lenght' => '1', 'pd_name' => 'Délka'],
+			['pd_id' => '103', 'pd_id_diffmode' => '2', 'pd_visible' => '1', 'pd_lenght' => '2', 'pd_name' => 'Velikost + Hrubost'],
+			['pd_id' => '105', 'pd_id_diffmode' => '2', 'pd_visible' => '1', 'pd_lenght' => '2', 'pd_name' => 'Rozměr + Hrubost'],
+			['pd_id' => '160', 'pd_id_diffmode' => '2', 'pd_visible' => '1', 'pd_lenght' => '2', 'pd_name' => 'Toto je test']
+		];
+		*/
 
-        DB::table('prod_difference')->insert([
-            'id'    => 1,
-            'count' => 0,
-            'name'  => 'Základní'
-        ]);
+		$prod_difference = [
+			['id' => '1', 'visible' => '1', 'count' => '0', 'name' => 'Základní'],
+			['id' => '11', 'visible' => '1', 'count' => '1', 'name' => 'Velikost'],
+			['id' => '13', 'visible' => '1', 'count' => '1', 'name' => 'Barva'],
+			['id' => '143', 'visible' => '1', 'count' => '2', 'name' => 'Velikost + Barva']
+		];
 
-        DB::table('prod_difference')->insert([
-            'id'    => 11,
-            'count' => 1,
-            'name'  => 'Velikost'
-        ]);
+		DB::table('prod_difference')->delete();
 
-        DB::table('prod_difference')->insert([
-            'id'    => 13,
-            'count' => 1,
-            'name'  => 'Barva'
-        ]);
-
-        DB::table('prod_difference')->insert([
-            'id'    => 143,
-            'count' => 2,
-            'name'  => 'Velikost + Barva'
-        ]);
-    }
+		foreach ($prod_difference as $row) {
+			DB::table('prod_difference')->insert([
+				'id'    => $row['id'],
+				'count' => $row['count'],
+				'name'  => $row['name']
+			]);
+		}
+	}
 }
--- app/database/seeds/ProdDifferenceSetSeeder.php
+++ app/database/seeds/ProdDifferenceSetSeeder.php
@@ -3,25 +3,37 @@
 class ProdDifferenceSetSeeder extends Seeder
 {
 
-    public function run()
-    {
-        $prod2difference2set = [
-            ['pds_id' => '1', 'pds_visible' => '1', 'pds_name' => '[A/N]', 'pds_sortby' => 'id'],
-            ['pds_id' => '11', 'pds_visible' => '1', 'pds_name' => 'Velikost', 'pds_sortby' => 'id'],
-            ['pds_id' => '13', 'pds_visible' => '1', 'pds_name' => 'Barva', 'pds_sortby' => 'name'],
-            ['pds_id' => '17', 'pds_visible' => '1', 'pds_name' => 'Rozměr', 'pds_sortby' => 'id'],
-            ['pds_id' => '23', 'pds_visible' => '1', 'pds_name' => 'Délka', 'pds_sortby' => 'id'],
-        ];
+	public function run()
+	{
+		// IN SHOP OLD
+		/*
+		$prod2difference2set = [
+			['pds_id' => '1', 'pds_visible' => '0', 'pds_name' => '[A/N]', 'pds_sortby' => 'pdv_id'],
+			['pds_id' => '10', 'pds_visible' => '1', 'pds_name' => 'Velikost', 'pds_sortby' => 'pdv_id'],
+			['pds_id' => '40', 'pds_visible' => '1', 'pds_name' => 'Rozměr', 'pds_sortby' => 'pdv_id'],
+			['pds_id' => '50', 'pds_visible' => '1', 'pds_name' => 'Hrubost', 'pds_sortby' => 'pdv_id'],
+			['pds_id' => '60', 'pds_visible' => '1', 'pds_name' => 'Velikost', 'pds_sortby' => 'pdv_id'],
+			['pds_id' => '70', 'pds_visible' => '1', 'pds_name' => 'Délka', 'pds_sortby' => 'pdv_id']
+		];
+		*/
 
-        DB::table('prod_difference_set')->delete();
+		$prod2difference2set = [
+			['pds_id' => '1', 'pds_visible' => '1', 'pds_name' => '[A/N]', 'pds_sortby' => 'id'],
+			['pds_id' => '11', 'pds_visible' => '1', 'pds_name' => 'Velikost', 'pds_sortby' => 'id'],
+			['pds_id' => '13', 'pds_visible' => '1', 'pds_name' => 'Barva', 'pds_sortby' => 'name'],
+			['pds_id' => '17', 'pds_visible' => '1', 'pds_name' => 'Rozměr', 'pds_sortby' => 'id'],
+			['pds_id' => '23', 'pds_visible' => '1', 'pds_name' => 'Délka', 'pds_sortby' => 'id'],
+		];
 
-        foreach ($prod2difference2set as $row) {
-            DB::table('prod_difference_set')->insert([
-                'id'     => $row['pds_id'],
-                'name'   => $row['pds_name'],
-                'sortby' => $row['pds_sortby']
-            ]);
-        }
+		DB::table('prod_difference_set')->delete();
 
-    }
+		foreach ($prod2difference2set as $row) {
+			DB::table('prod_difference_set')->insert([
+				'id'     => $row['pds_id'],
+				'name'   => $row['pds_name'],
+				'sortby' => $row['pds_sortby']
+			]);
+		}
+
+	}
 }
