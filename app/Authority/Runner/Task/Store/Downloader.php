643325be2fab77dd6c1f422a7bcd07affbf4d791
radoshordos <radoshordos@gmail.com>
2015-03-31 10:45:12 +0200

    Guru downloader

--- app/Authority/Runner/Task/Store/Downloader.php
+++ app/Authority/Runner/Task/Store/Downloader.php
@@ -2,35 +2,37 @@
 
 class Downloader
 {
-	protected $dir;
-	protected $file;
-	protected $source;
+    protected $dir;
+    protected $file;
+    protected $source;
 
-	function __construct($dir, $file, $source)
-	{
-		$this->dir = $dir;
-		$this->file = $file;
-		$this->source = $source;
-	}
+    function __construct($dir, $file, $source)
+    {
+        $this->dir = $dir;
+        $this->file = $file;
+        $this->source = $source;
+    }
 
-	function runDownload($file_get_context = true)
-	{
-		if ($file_get_context == true) {
-			file_put_contents($this->dir . $this->file, file_get_contents($this->source));
-		} else {
-			file_put_contents($this->dir . $this->file, $this->source);
-		}
-	}
+    function runDownload($file_get_context = TRUE)
+    {
+        if ($file_get_context == TRUE) {
+            $opts = ['http' => ['header' => "User-Agent: Guru-Downloader/1.0\r\n"]];
+            $context = stream_context_create($opts);
+            file_put_contents($this->dir . $this->file, file_get_contents($this->source, FALSE, $context));
+        } else {
+            file_put_contents($this->dir . $this->file, $this->source);
+        }
+    }
 
-	function unzipDownload()
-	{
-		$zip = new \ZipArchive();
-		$result = $zip->open($this->dir . $this->file);
-		if ($result === true) {
-			$zip->extractTo($this->dir);
-			$zip->close();
-			return true;
-		}
-		return false;
-	}
+    function unzipDownload()
+    {
+        $zip = new \ZipArchive();
+        $result = $zip->open($this->dir . $this->file);
+        if ($result === TRUE) {
+            $zip->extractTo($this->dir);
+            $zip->close();
+            return TRUE;
+        }
+        return FALSE;
+    }
 }
