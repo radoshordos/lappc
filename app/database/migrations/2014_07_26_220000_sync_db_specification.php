86bce32fd264ba470f1da9db0188361315e0fffe
RadosHordos <radoshordos@gmail.com>
2015-04-01 23:36:10 +0200

    SyncDbSpecification

--- app/database/migrations/2014_07_26_220000_sync_db_specification.php
+++ app/database/migrations/2014_07_26_220000_sync_db_specification.php
@@ -9,10 +9,12 @@ class SyncDbSpecification extends Migration
     {
         Schema::create('sync_db_specification', function (Blueprint $table) {
             $table->increments('id')->unsigned();
+            $table->smallInteger('media_variations_id')->unsigned();
             $table->integer('sync_id')->unsigned();
             $table->string('label', '255');
             $table->engine = 'InnoDB';
 
+            $table->foreign('media_variations_id')->references('id')->on('media_variations')->onUpdate('cascade')->onDelete('no action');
             $table->foreign('sync_id')->references('id')->on('sync_db')->onUpdate('cascade')->onDelete('cascade');
         });
     }
