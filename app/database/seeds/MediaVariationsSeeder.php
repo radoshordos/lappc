9124c0166ff1355b8a6f71e8bbb1e69fcfa11af8
radoshordos <radoshordos@gmail.com>
2015-04-03 15:02:55 +0200

    Media variations

--- app/database/seeds/MediaVariationsSeeder.php
+++ app/database/seeds/MediaVariationsSeeder.php
@@ -5,9 +5,9 @@ class MediaVariationsSeeder extends Seeder
     public function run()
     {
         $media2variace = [
-            ['mv_id' => '1', 'mv_id_type' => '0', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '0', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => '[NULL]'],
-            ['mv_id' => '101', 'mv_id_type' => '1', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '0', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Pouze text'],
-            ['mv_id' => '102', 'mv_id_type' => '1', 'mv_visible_rule' => '0', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '0', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Text => Aktuality'],
+            ['mv_id' => '1', 'mv_id_type' => '0', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '0', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => '[NULL]'],
+            ['mv_id' => '101', 'mv_id_type' => '1', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '0', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Pouze text'],
+            ['mv_id' => '102', 'mv_id_type' => '1', 'mv_visible_rule' => '0', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '0', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Text => Aktuality'],
 
             ['mv_id' => '201', 'mv_id_type' => '2', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '1', 'mv_visible_prod' => '0', 'mv_tag' => 'DOC', 'mv_class' => 'type-doc', 'mv_icon' => 'ico_doc.gif', 'mv_name' => 'Document (*.DOC, *.DOCX)'],
             ['mv_id' => '202', 'mv_id_type' => '2', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '1', 'mv_visible_prod' => '0', 'mv_tag' => 'PDF', 'mv_class' => 'type-pdf', 'mv_icon' => 'ico_pdf.gif', 'mv_name' => 'Portable Document Format (*.PDF)'],
@@ -15,13 +15,13 @@ class MediaVariationsSeeder extends Seeder
             ['mv_id' => '301', 'mv_id_type' => '3', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '1', 'mv_visible_prod' => '0', 'mv_tag' => 'YOUTUBE', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Youtube.com (Video stream)'],
 
             ['mv_id' => '601', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => 'PARAMETERS', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Technická data'],
-            ['mv_id' => '602', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Vybavení a charakteristika'],
+            ['mv_id' => '602', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Vybavení a charakteristika'],
             ['mv_id' => '603', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => 'DESCRIPTIONS', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Popis'],
             ['mv_id' => '604', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => 'PACKAGECONTENTS', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Rozsah dodávky'],
-            ['mv_id' => '605', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Přednosti výrobku'],
-            ['mv_id' => '606', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Používá se pro'],
-            ['mv_id' => '608', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Obsah balení'],
-            ['mv_id' => '609', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Doporučujeme']
+            ['mv_id' => '605', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Přednosti výrobku'],
+            ['mv_id' => '606', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Používá se pro'],
+            ['mv_id' => '608', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Obsah balení'],
+            ['mv_id' => '609', 'mv_id_type' => '6', 'mv_visible_rule' => '1', 'mv_visible_group' => '0', 'mv_visible_media' => '0', 'mv_visible_prod' => '1', 'mv_tag' => NULL, 'mv_class' => NULL, 'mv_icon' => NULL, 'mv_name' => 'Doporučujeme']
         ];
 
         DB::table('media_variations')->delete();
