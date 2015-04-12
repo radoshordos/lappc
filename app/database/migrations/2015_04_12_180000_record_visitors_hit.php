40767af5d680668c8ee4d06e8a0d34ffd576b067
RadosHordos <radoshordos@gmail.com>
2015-04-12 20:16:20 +0200

    record_visitors_hit

---
+++ app/database/migrations/2015_04_12_180000_record_visitors_hit.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RecordVisitorsHit extends Migration
+{
+    public function up()
+    {
+        Schema::create('record_visitors_hit', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('created_at');
+            $table->string('http_referer', 255)->nullable();
+            $table->string('request_url', 255)->nullable();
+            $table->string('remote_addr', 255)->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('record_visitors_hit');
+    }
+}
