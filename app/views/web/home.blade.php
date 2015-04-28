cc6d1543c76785ff4535e0d34e5b918df9e90644
RadosHordos <radoshordos@gmail.com>
2015-04-28 22:05:52 +0200

    callout-card

--- app/views/web/home.blade.php
+++ app/views/web/home.blade.php
@@ -7,16 +7,116 @@
 			@include('web.global.leftmenu')
 		</div>
 		<div id="part-right" class="large-14 columns">
-			<div class="" style="background-color: #074f69;padding: 20px 30px;">
 
-				<div class="row">
-					<section class="large-18 columns">
-						<i style="color: #008cba;" class="fa-2x pull-left fa fa-bullhorn"></i>
 
-						<h3 style="color: #ffffff;">Tady Vám s nákupem poradíme</h3>
+			<div class="row">
+				<div class="medium-9 large-6 columns">
+					<div class="callout-card success radius">
+						<div class="card-label">
+							<div class="label-text">
+								Win!
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead"> Mad Max: Fury Road</h3>
 
-						<p style="color: #ffffff;">Tisíce položek skladem a zítra u Vás doma.</p>
-					</section>
+							<p>In a post-apocalyptic world, in which people fight to the death, Max teams up with a mysterious woman, Furiousa, to try and survive.</p>
+						</div>
+					</div>
+				</div>
+				<div class="medium-9 large-6 columns">
+					<div class="callout-card primary">
+						<div class="card-label">
+							<div class="label-text">
+								Blue
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead">Star Wars: Episode VII - The Force Awakens</h3>
+
+							<p>A continuation of the saga created by George Lucas set thirty years after Star Wars: Episode VI - Return of the Jedi.</p>
+						</div>
+					</div>
+				</div>
+				<div class="medium-9 large-6 columns">
+					<div class="callout-card info">
+						<div class="card-label">
+							<div class="label-text">
+								AKCE
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead">Chappie</h3>
+
+							<p>In the near future, crime is patrolled by a mechanized police force. When one police droid, Chappie, is stolen and given new programming, he becomes the first robot with the ability to think and feel for
+								himself.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="small-12 medium-9 columns">
+					<div class="callout-card alert radius">
+						<div class="card-label">
+							<div class="label-text">
+								Stop
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead"> Poltergeist</h3>
+
+							<p>A family's suburban home is invaded by evil forces.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="small-12 medium-9 columns">
+					<div class="callout-card warning radius">
+						<div class="card-label">
+							<div class="label-text">
+								Hey!
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead">Jurassic World</h3>
+
+							<p>Twenty-two years after the events of Jurassic Park, Isla Nublar now features a fully functioning dinosaur theme park, Jurassic World, as originally envisioned by John Hammond. After 10 years of operation and visitor
+								rates declining, in order to fulfill a corporate mandate, a new attraction is created to re-spark visitor's interest, which backfires horribly.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="medium-12 columns">
+					<div class="callout-card">
+						<div class="card-label">
+							<div class="label-text">
+								Yo!
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead"> Avengers: Age of Ultron </h3>
+
+							<p>When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and it is up to the Avengers to stop the villainous Ultron from enacting his terrible plans.</p>
+						</div>
+					</div>
+				</div>
+				<div class="medium-6 columns">
+					<div class="callout-card alert">
+						<div class="card-label">
+							<div class="label-text">
+								TOP 1
+							</div>
+						</div>
+						<div class="callout-card-content">
+							<h3 class="lead">Makita BUM168Z</h3>
+							<img src="/web/naradi/aku-program/makita-uh200dz.jpg" alt="death star"/>
+							<p>Akumulátorová okružní pila Makita BSS500RFE 14,4V Li-Ion, 3Ah</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
