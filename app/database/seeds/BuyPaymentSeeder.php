d74630ce24269e5dab5c105b093c33cf5f5eaca1
RadosHordos <radoshordos@gmail.com>
2015-06-15 23:36:48 +0200

    Multiservis

--- app/database/seeds/BuyPaymentSeeder.php
+++ app/database/seeds/BuyPaymentSeeder.php
@@ -2,54 +2,63 @@
 
 class BuyPaymentSeeder extends Seeder
 {
-	public function run()
-	{
-		$i = 1;
-		DB::table('buy_payment')->delete();
+    public function run()
+    {
+        $i = 1;
+        DB::table('buy_payment')->delete();
 
-		DB::table('buy_payment')->insert([
-			'id'              => $i++,
-			'payment_type_id' => 3,
-			'alias'           => 'platebni-karta',
-			'name'            => 'Platba kartou předem',
+        DB::table('buy_payment')->insert([
+            'id'              => $i++,
+            'payment_type_id' => 3,
+            'alias'           => 'platebni-karta',
+            'name'            => 'Platba kartou předem',
             'name_short'      => 'Kartou',
-			'price_payment'   => 0
-		]);
+            'price_payment'   => 0
+        ]);
 
-		DB::table('buy_payment')->insert([
-			'id'              => $i++,
-			'payment_type_id' => 4,
-			'alias'           => 'online-platba',
-			'name'            => 'Online platba',
+        DB::table('buy_payment')->insert([
+            'id'              => $i++,
+            'payment_type_id' => 4,
+            'alias'           => 'online-platba',
+            'name'            => 'Online platba',
             'name_short'      => 'Online',
-			'price_payment'   => 0
-		]);
+            'price_payment'   => 0
+        ]);
 
-		DB::table('buy_payment')->insert([
-			'id'              => $i++,
-			'payment_type_id' => 4,
-			'alias'           => 'bankovni-prevod',
-			'name'            => 'Bankovní převod',
+        DB::table('buy_payment')->insert([
+            'id'              => $i++,
+            'payment_type_id' => 4,
+            'alias'           => 'bankovni-prevod',
+            'name'            => 'Bankovní převod',
             'name_short'      => 'Převod',
-			'price_payment'   => 0
-		]);
+            'price_payment'   => 0
+        ]);
 
-		DB::table('buy_payment')->insert([
-			'id'              => $i++,
-			'payment_type_id' => 1,
-			'alias'           => 'platba-dobirkou',
-			'name'            => 'Platba dobírkou',
+        DB::table('buy_payment')->insert([
+            'id'              => $i++,
+            'payment_type_id' => 1,
+            'alias'           => 'platba-dobirkou',
+            'name'            => 'Platba dobírkou',
             'name_short'      => 'Dobírka',
-			'price_payment'   => 30
-		]);
+            'price_payment'   => 30
+        ]);
 
-		DB::table('buy_payment')->insert([
-			'id'              => $i++,
-			'payment_type_id' => 2,
-			'alias'           => 'osobni-prevzeti',
-			'name'            => 'Při osobním převzetí (hotově, kartou)',
+        DB::table('buy_payment')->insert([
+            'id'              => $i++,
+            'payment_type_id' => 2,
+            'alias'           => 'osobni-prevzeti',
+            'name'            => 'Při osobním převzetí (hotově, kartou)',
             'name_short'      => 'Osobně',
-			'price_payment'   => 0
-		]);
-	}
+            'price_payment'   => 0
+        ]);
+
+        DB::table('buy_payment')->insert([
+            'id'              => $i++,
+            'payment_type_id' => 2,
+            'alias'           => 'splatky-multiservis',
+            'name'            => 'Splátky Multiservis',
+            'name_short'      => 'Splátky',
+            'price_payment'   => 0
+        ]);
+    }
 }
--- app/views/web/prod/panel_price.blade.php
+++ app/views/web/prod/panel_price.blade.php
@@ -1,26 +1,34 @@
 <div id="price-panel" class="panel callout radius">
-	<div class="row">
-		<div class="label-list">
-			@if ($vpa->getPrice() >= 3000)
-				<span class="label">Doprava ZDARMA</span>
-			@endif
-		</div>
-	</div>
+    <div class="row">
+        <div class="label-list">
+            @if ($vpa->getPrice() >= 3000)
+                <span class="label">Doprava ZDARMA</span>
+            @endif
+        </div>
+    </div>
 
-	<div class="row">
-		<div class="medium-10 columns">
-			<h5>Vaše cena: <strong data-tooltip aria-haspopup="true" class="has-tip radius" title="Cena bez DPH: {{ $vpa->priceFormatCurrencyWithout(); }}">{{ $vpa->priceFormatCurrencyWith(); }}</strong></h5>
-			<p>včetně DPH a všech poplatků</p>
-		</div>
+    <div class="row">
+        <div class="medium-10 columns">
+            <h5>Vaše cena: <strong data-tooltip aria-haspopup="true" class="has-tip radius" title="Cena bez DPH: {{ $vpa->priceFormatCurrencyWithout(); }}">{{ $vpa->priceFormatCurrencyWith(); }}</strong></h5>
 
-		<div class="row">
-			<div class="medium-8 columns">
-				@if (!empty($item_row))
-					{{ Form::open(['action' => 'KosikController@store','class' => 'form-horizontal', 'role' => 'form']) }}
-					<input name="do-kosiku[{{ $item_row->id }}]" style="max-width: 12em" class="success button expand" type="submit" title="Vložit {{ $vpa->getProdNameWithBonus(); }} do košíku" value="Do košíku"/>
-					{{ Form::close() }}
-				@endif
-			</div>
-		</div>
-	</div>
+            <p>včetně DPH a všech poplatků</p>
+        </div>
+
+        <div class="row">
+            <div class="medium-8 columns">
+                @if (!empty($item_row))
+                    {{ Form::open(['action' => 'KosikController@store','class' => 'form-horizontal', 'role' => 'form']) }}
+                    <input name="do-kosiku[{{ $item_row->id }}]" style="max-width: 12em" class="success button expand" type="submit" title="Vložit {{ $vpa->getProdNameWithBonus(); }} do košíku" value="Do košíku"/>
+                    {{ Form::close() }}
+                @endif
+            </div>
+        </div>
+    </div>
+
+    <a href="#" data-reveal-id="firstModal" class="radius button">Splátková kalkulačka</a>
+
+    <div id="firstModal" class="reveal-modal" data-reveal aria-labelledby="firstModalTitle" seamless="false" aria-hidden="true" role="dialog">
+        <iframe name="Splátková kalkulačka Homecredit" class="medium-18 columns" style="height: 50em"
+                src="https://i-calc-train.homecredit.net/icalc/entry.do?shop=0999&o_price=10000&time_request=15.06.2015-20:15:08&sh=588e1b6d347b8d39776a065b499d0838"></iframe>
+    </div>
 </div>
