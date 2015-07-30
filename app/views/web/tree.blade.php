901c2499ffb357c751fc2caf89edd50b256dda38
radoshordos <radoshordos@gmail.com>
2015-07-30 13:49:26 +0200

    zurb

--- app/views/web/tree.blade.php
+++ app/views/web/tree.blade.php
@@ -9,17 +9,18 @@
             </div>
             <div id="part-right" class="medium-14 large-14 columns">
                 <h1>{{ $view_tree_actual['tree_desc'] }}</h1>
-
                 <div id="tree-panel" class="small-18 panel radius clearfix valign-middle">
                     <div class="row">
                         @include('web.tree.boxdev')
                     </div>
-                    <div class="row" style="border-top: 5px solid #ffffff">
-                        <div id="prod-filter" class="small-3 columns">
+                    <div class="row">
+                        <div id="prod-filter" class="small-10 medium-6 large-6 columns">
                             @include('web.tree.blockprodfilter')
                         </div>
-                        <div id="prod-sort" class="small-6 columns">
-                            @include('web.tree.boxsorting')
+                        <div id="prod-sort" class="small-8 medium-12 large-11 columns">
+                            <div class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">
+                                @include('web.tree.boxsorting')
+                            </div>
                         </div>
                     </div>
                 </div>
